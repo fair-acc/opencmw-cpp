f69e367e273e8fdf31d6dba04e323fc5ce66a42c
Patricia Aas <patricia@turtlesec.no>
2021-03-29 13:33:47 +0200

    Run clang format on the example

--- src/benchmarks/refl-cpp/refl-cpp.cpp
+++ src/benchmarks/refl-cpp/refl-cpp.cpp
@@ -5,32 +5,28 @@
  * detects the presence of this attribute and selectively serializes
  * the members of the provided value.
  */
-#include <iostream>
-#include <cstring>
-#include <string>
-#include <sstream>
-#include <vector>
+#include "refl.hpp"
 #include <any>
 #include <cassert>
-#include <unordered_map>
 #include <cmath>
-#include "refl.hpp"
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 // define an empty maker attribute to be used on fields and functions only
-struct serializable : refl::attr::usage::field, refl::attr::usage::function
-{
+struct serializable : refl::attr::usage::field, refl::attr::usage::function {
 };
 
-template <typename T>
-void serialize(std::ostream& os, T&& value)
-{
+template<typename T>
+void serialize(std::ostream &os, T &&value) {
     // iterate over the members of T
-    for_each(refl::reflect(value).members, [&](auto member)
-    {
+    for_each(refl::reflect(value).members, [&](auto member) {
         // is_readable checks if the member is a non-const field
         // or a 0-arg const-qualified function marked with property attribute
-        if constexpr (is_readable(member) && refl::descriptor::has_attribute<serializable>(member))
-        {
+        if constexpr (is_readable(member) && refl::descriptor::has_attribute<serializable>(member)) {
             // get_display_name prefers the friendly_name of the property over the function name
             os << get_display_name(member) << "=";
             // member(value) returns a reference to the field or calls the property accessor
@@ -39,19 +35,16 @@ void serialize(std::ostream& os, T&& value)
     });
 }
 
-struct Point
-{
+struct Point {
     float x;
     float y;
 
-    float magnitude() const
-    {
+    float magnitude() const {
         return std::sqrt(x * x + y * y);
     }
 };
 
-void debug_point(std::ostream& os, const Point& pt)
-{
+void debug_point(std::ostream &os, const Point &pt) {
     os << "(" << pt.x << ", " << pt.y << ")";
 }
 
@@ -65,13 +58,12 @@ REFL_FIELD(y, serializable())
 REFL_FUNC(magnitude)
 REFL_END
 
-int main()
-{
+int main() {
     std::cout << "Custom serialization: ";
     serialize(std::cout, Point{ 1, 1 });
 
     std::cout << "\nBuilt-in debug (supports arbitrary containers): ";
-    std::vector<Point> pts{ {0,1}, {1,0} };
+    std::vector<Point> pts{ { 0, 1 }, { 1, 0 } };
 
     // debug will print the debug representation of the value to the stream;
     // for types with begin() and end(), refl-cpp will automatically
