97bba2fdeb1d39c7e3e1b84e573decbacd6efe19
Patricia Aas <patricia@turtlesec.no>
2021-03-30 13:51:42 +0200

    Use latest master to compile on MacOS

--- cmake/ReflCpp.cmake
+++ cmake/ReflCpp.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
         refl-cpp
         GIT_REPOSITORY https://github.com/veselink1/refl-cpp.git
-        GIT_TAG        v0.12.1
+        GIT_TAG        f192b08e06f934dc5826b7650c14dd88b1baaccf
 )
 FetchContent_MakeAvailable(refl-cpp)
