434589685880790ac1f2613d06e4806eea419f51
Patricia Aas <patricia@turtlesec.no>
2021-03-30 13:43:40 +0200

    Use CMake FetchContent to get refl-cpp

---
+++ cmake/ReflCpp.cmake
@@ -0,0 +1,7 @@
+include(FetchContent)
+FetchContent_Declare(
+        refl-cpp
+        GIT_REPOSITORY https://github.com/veselink1/refl-cpp.git
+        GIT_TAG        v0.12.1
+)
+FetchContent_MakeAvailable(refl-cpp)
