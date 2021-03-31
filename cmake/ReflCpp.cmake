5a59796fc2825e16e56d62b17a855cf49a686bc0
Patricia Aas <psmaas@gmail.com>
2021-03-31 15:10:14 +0200

    Simple hello world using refl-cpp
    * Simple hello world using refl-cpp
    * Ignore dependencies in code coverage

---
+++ cmake/ReflCpp.cmake
@@ -0,0 +1,7 @@
+include(FetchContent)
+FetchContent_Declare(
+        refl-cpp
+        GIT_REPOSITORY https://github.com/veselink1/refl-cpp.git
+        GIT_TAG        f192b08e06f934dc5826b7650c14dd88b1baaccf
+)
+FetchContent_MakeAvailable(refl-cpp)
