19dc1d4c00bbde1a31fcdf848519780ab66b976a
Alexander Krimm <A.Krimm@gsi.de>
2021-05-28 15:33:34 +0200

    MultiArray: first implementation

---
+++ include/MultiArray.hpp
@@ -0,0 +1,297 @@
+#ifndef OPENCMW_CPP_MULTI_ARRAY_HPP
+#define OPENCMW_CPP_MULTI_ARRAY_HPP
+
+#include <fmt/color.h>
+#include <fmt/format.h>
+#include <array>
+#include <cstddef>  // std::size_t
+#include <iostream> // std::cout
+#include <vector>
+
+namespace opencmw {
+
+/// MultiArray implementation with a fixed number of dimensions but variable number of elements
+/// \tparam T type of the data entries
+/// \tparam n_dims number of dimensions >= 1 (1 -> vector, 2-> matrix, etc)
+template<typename T, uint32_t n_dims>
+requires(n_dims >= 1) class MultiArray {
+public:
+    using size_t_ = uint32_t; // alternative: using size_t_ = size_t; // have to use signed int here?, because that's what is used in the serialised protocol (bc of java)
+    using value_type = T;
+    static constexpr size_t_ n_dims_ = n_dims;
+private:
+    std::vector<value_type>              elements_;  // data
+    size_t_                     n_element_; // number of valid entries in data
+    std::array<size_t_, n_dims> dims_;      // sizes for every dimension
+    std::array<size_t_, n_dims> strides_;   // strides_ for every dimension
+    std::array<size_t_, n_dims> offsets_;   // offset for every dimension
+public:
+    /// full control constructor, allowing to realise custom matrix layouts
+    [[nodiscard]] constexpr MultiArray(
+            const std::vector<value_type>               elements,
+            const std::array<size_t_, n_dims> &dimensions,
+            const std::array<size_t_, n_dims> &strides,
+            const std::array<size_t_, n_dims>  offsets)
+        : elements_(elements), n_element_(size_t_(elements.size())), dims_(dimensions), strides_(strides), offsets_(offsets) {}
+
+    /// copy constructor for wrapping a linear vector of elements
+    /// \param elements the elements in row major format
+    /// \param dimensions the size of the MultiArray in every dimension
+    [[nodiscard]] constexpr MultiArray(const std::vector<value_type> elements, const std::array<size_t_, n_dims> &dimensions)
+        : elements_(elements.data(), elements.size()), n_element_(dimensions[n_dims - 1]), dims_(dimensions), strides_(), offsets_() {
+        strides_[n_dims - 1] = 1;
+        for (auto i = n_dims - 1; i > 0; i--) {
+            n_element_ *= dimensions[i - 1];
+            strides_[i - 1] = strides_[i] * dimensions[i];
+        }
+    }
+
+    /// move constructor
+    /// \param elements the elements in row major format
+    /// \param dimensions the size of the MultiArray in every dimension
+    [[nodiscard]] constexpr MultiArray(const std::vector<value_type> &&elements, const std::array<size_t_, n_dims> &&dimensions)
+        : elements_(std::move(elements)), n_element_(dimensions[n_dims - 1]), dims_(std::move(dimensions)), strides_(), offsets_() {
+        strides_[n_dims - 1] = 1;
+        for (auto i = n_dims - 1; i > 0; i--) {
+            n_element_ *= dimensions[i - 1];
+            strides_[i - 1] = strides_[i] * dimensions[i];
+        }
+    }
+
+    /// constructor for an empty MultiArray of given size
+    /// \param dimensions the size of the MultiArray in every dimension
+    [[nodiscard]] constexpr explicit MultiArray(const std::array<size_t_, n_dims> &dimensions)
+        : n_element_(dimensions[n_dims - 1]), dims_(dimensions), strides_(), offsets_() {
+        strides_[n_dims - 1] = 1;
+        for (auto i = n_dims - 1; i > 0; i--) {
+            n_element_ *= dimensions[static_cast<unsigned long>(i - 1)];
+            strides_[i - 1] = strides_[i] * dimensions[i];
+        }
+        elements_ = std::vector<value_type>(n_element_);
+    }
+
+    /// \return the number of dimensions of the MultiArray
+    [[nodiscard]] constexpr const auto &dimensions() const noexcept {
+        return dims_;
+    }
+
+    /// \return the mutable numbers of dimensions of the MultiArray
+    [[nodiscard]] constexpr auto &&dimensions() noexcept {
+        return dims_;
+    }
+
+    /// \return the raw vector with the elements of the MultiArray
+    [[nodiscard]] constexpr const auto &elements() const noexcept {
+        return elements_;
+    }
+
+    /// \return the mutable raw vector with the elements of the MultiArray
+    [[nodiscard]] constexpr auto &&elements() noexcept {
+        return elements_;
+    }
+
+    /// \return the number of valid entries in the raw vector
+    [[nodiscard]] constexpr auto &element_count() noexcept {
+        return n_element_;
+    }
+    [[nodiscard]] constexpr auto &element_count() const noexcept {
+        return n_element_;
+    }
+
+    /// \param dim the dimension to return the offset for
+    /// \return the offset of the data in the raw array for the given dimension
+    [[nodiscard]] constexpr auto &offset(size_t_ dim) noexcept {
+        return offsets_[dim];
+    }
+    [[nodiscard]] constexpr auto &offset(size_t_ dim) const noexcept {
+        return offsets_[dim];
+    }
+
+    /// \param dim the dimension to return the stride for
+    /// \return the stride of the data in the raw array for the given dimension
+    [[nodiscard]] constexpr auto &stride(size_t_ dim) noexcept {
+        return strides_[dim];
+    }
+    [[nodiscard]] constexpr auto &stride(size_t_ dim) const noexcept {
+        return strides_[dim];
+    }
+
+    /// \param dim the dimension to return the size for
+    /// \return the number of rows/columns/slices/... of the data in the raw array for the given dimension
+    [[nodiscard]] constexpr const auto &n(size_t_ dim) const noexcept {
+        return dims_[dim];
+    }
+
+    /// compute the scalar index of an entry in the MultiArray
+    /// \param indices indices for every dimension
+    /// \return a scalar index in the multi array
+    [[nodiscard]] constexpr size_t_ index(const std::array<size_t_, n_dims> indices) const noexcept {
+        size_t_ index = 0;
+        for (size_t_ i = 0; i < n_dims; i++) {
+            index += (offsets_[i] + indices[i]) * strides_[i];
+        }
+        return index;
+    }
+
+    /// compute the multi-index from the scalar index in the backing data
+    /// \param index scalar index, has to be a position with valid data
+    /// \return corresponding multi index
+    [[nodiscard]] constexpr std::array<size_t_, n_dims> indices(const size_t_ index) const noexcept {
+        std::array<size_t_, n_dims> indices{};
+        size_t_                     index_ = index;
+        for (size_t_ i = 0; i < n_dims; i++) {
+            index_ -= offsets_[i];
+            indices[i] = index_ / strides_[i];
+            index_ -= indices[i] * strides_[i];
+        }
+        return indices;
+    }
+
+    /// access operator for linear access
+    [[nodiscard]] constexpr value_type &operator[](const size_t_ index) {
+        return elements_[index];
+    }
+
+    /// access operator for multiple dimensions
+    [[nodiscard]] constexpr value_type &operator[](const std::array<size_t_, n_dims> indices) {
+        return elements_[index(indices)];
+    }
+
+    /// access operator for linear access (parentheses version)
+    [[nodiscard]] constexpr value_type &operator()(const size_t_ index) {
+        return elements_[index];
+    }
+
+    /// allow access with parenthesis operator (no curly braces)
+    template<typename... R, typename std::enable_if<sizeof...(R) == n_dims && (true && ... && std::convertible_to<R, size_t_>), size_t_>::type = 0>
+    [[nodiscard]] constexpr value_type &operator()(const R... indices) {
+        return elements_[index(std::array{ indices... })];
+    }
+
+    /// explicit method linear accessor (bounds checked)
+    [[nodiscard]] constexpr value_type &get(const size_t_ index) {
+        return elements_.at(index);
+    }
+
+    /// explicit method multi array accessor (bounds checked)
+    [[nodiscard]] constexpr value_type &get(const std::array<size_t_, n_dims> indices) {
+        return elements_.at(index(indices));
+    }
+    template<typename... R, typename std::enable_if<sizeof...(R) == n_dims && (true && ... && std::convertible_to<R, size_t_>), size_t_>::type = 0>
+    [[nodiscard]] constexpr value_type &get(const R... indices) {
+        return elements_.at(index(std::array{ indices... }));
+    }
+
+    // math operator overloads [+-*/[=]] arithmetic<value_type> MultiArray<value_type> vector<value_type> (both lvalue and rvalue version)
+    MultiArray &operator+=(const MultiArray<value_type , n_dims> operand) {
+        // todo: verify dimension match. Allow broadcasting (adding a column vector to each row or similar)?
+        for (size_t_ i = 0; i < n_element_; i++) { // todo: use iterator to only change valid fields
+            this[i] += operand[i];
+        }
+        return *this;
+    }
+    template<typename R>
+    requires std::is_arithmetic<value_type>::value
+            MultiArray &
+            operator+=(const R operand) {
+        for (size_t_ i = 0; i < n_element_; i++) { // todo: use iterator to only change valid fields
+            this[i] += operand;
+        }
+        return *this;
+    }
+    MultiArray<value_type , n_dims> operator+(const MultiArray<value_type, n_dims> operand) {
+        // todo: verify dimension match. Allow broadcasting (adding a column vector to each row or similar)?
+        MultiArray &result = MultiArray<value_type, n_dims>(this->dims_);
+        for (size_t_ i = 0; i < n_element_; i++) { // todo: use iterator to only change valid fields
+            result[i] = this[i] + operand[i];
+        }
+        return result;
+    }
+    template<typename R>
+    requires std::is_arithmetic<value_type>::value
+            MultiArray &
+            operator+(const R operand) {
+        MultiArray &result = MultiArray<value_type, n_dims>(this->dims_);
+        for (size_t_ i = 0; i < n_element_; i++) { // todo: use iterator to only change valid fields
+            this[i] += operand;
+        }
+        return result;
+    }
+
+     bool operator==(const MultiArray<value_type,n_dims>& other) const noexcept {
+        // check if array is the same size
+        for (size_t_ i = 0; i < n_dims; i++) {
+            if (dims_[i] != other.dims_[i]) {
+                return false;
+            }
+        }
+        // check content of array todo: respect offsets/strides/etc
+        for (size_t_ i = 0; i < n_element_; i++) {
+            if (elements_[i] != other.elements_[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // lambda function on a per cell operation
+
+    // iterators todo: research possibility to have multidimensional iterators. nested iterators?
+    // array.iterator(1[,0])->iterator(1[,2]) // column iterator starting at the third entry of the first row
+    // forward/backward/const iterator -> enables ranges, <algorithm>, and <numeric>
+
+    // sub-arrays / slices / compact array
+    /// extract a row/column from an array
+    // constexpr MultiArrayView<value_type, n_dims - 1> slice(const size_t_ nDim, const size_t_ index) {
+    //     auto new_dims = std::array<size_t_, n_dims - 1>;
+    //     auto new_offsets = std::array<size_t_, n_dims - 1>;
+    //     for (size_t_ i = 0; i < n_dims -1; i++) {
+    //         if (i < nDim) {
+    //             new_dims[i] = dims_[i];
+    //         } else {
+    //             new_dims[i] = dims_[i+1];
+    //         }
+    //     }
+
+    //     return MultiArray(elements(), new_dims, new_offsets);
+    //     // return MultiArray(elements_, ); // todo: implement logic
+    // }
+    /// extract a sub-matrix
+    //constexpr MultiArrayView<value_type, n_dims> sub_array(const std::array<size_t, n_dims> min_indices, const std::array<size_t, n_dims> max_indices) {
+    //    return nullptr;
+    //}
+    /// sample down a matrix by skipping every nth value in every dimension
+    //constexpr MultiArrayView<value_type, n_dims> downsample(const std::array<size_t, n_dims> strides_) {
+    //    return nullptr;
+    //}
+
+    /// simple print operator for debugging
+    constexpr friend std::ostream &operator<<(std::ostream &output, const MultiArray &array) {
+        //return output << fmt::format("elements: {} dimensions: {} data: {}", array.element_count(), array.dimensions(), array.elements()); // does not work
+        output << "elements: " << array.element_count() << ", dimensions: ";
+        for (const auto &n : array.dimensions()) {
+            output << n << ", ";
+        }
+        output << " data: ";
+        for (const auto &e : array.elements()) {
+            output << e << ", ";
+        }
+        return output;
+    }
+};
+
+// MultiArrayView -> uses data from Multi Array, but with different layout/number of dimensions -> can represent slices and sampled down views of the data
+// multiArray          // 3d array
+//    .slice(1,3)      // get 3rd column
+//    .subArray(0,5,7) // get 5. to 7. row
+//    .downsample(2,2) // only use every other sample in z direction
+// views can be assigned to and this will affect the original array, so you can use this to blank out/initialize specific matrix elements
+// materialise a view into its own MultiArray by using the MultiArray(MultiArrayView) constructor
+
+// MultiArrayIterator -> returns iterator of T
+// MultiArrayDimIterator -> returns iterator of MultiArrayView<T, dim-1>
+// iterators should be aware if they represent congruent memory to allow using memmove type copying if applicable
+
+} // namespace opencmw
+
+#endif //OPENCMW_CPP_MULTI_ARRAY_HPP
---
+++ test/MultiArray_tests.cpp
@@ -0,0 +1,95 @@
+#include "MultiArray.hpp"
+#include <catch2/catch.hpp>
+#include <iostream>
+
+using namespace opencmw;
+
+TEST_CASE("MultiArray.doubleSimple", "[MultiArray]") {
+    MultiArray<double, 2> test({1,2,3,4,5,6}, {2,3});
+    // test that internal fields where computed correctly
+    REQUIRE(2 == test.n(0));
+    REQUIRE(3 == test.n(1));
+    REQUIRE(3 == test.stride(0));
+    REQUIRE(1 == test.stride(1));
+    REQUIRE(0 == test.offset(0));
+    REQUIRE(0 == test.offset(1));
+    // test index computation
+    REQUIRE(0 == test.index({0,0}));
+    REQUIRE(1 == test.index({0,1}));
+    REQUIRE(2 == test.index({0,2}));
+    REQUIRE(3 == test.index({1,0}));
+    REQUIRE(4 == test.index({1,1}));
+    REQUIRE(5 == test.index({1,2}));
+    // test reverse index computation
+    REQUIRE(std::array<uint32_t, 2>{0,0} == test.indices(0));
+    REQUIRE(std::array<uint32_t, 2>{0,1} == test.indices(1));
+    REQUIRE(std::array<uint32_t, 2>{0,2} == test.indices(2));
+    REQUIRE(std::array<uint32_t, 2>{1,0} == test.indices(3));
+    REQUIRE(std::array<uint32_t, 2>{1,1} == test.indices(4));
+    REQUIRE(std::array<uint32_t, 2>{1,2} == test.indices(5));
+    // multi index access
+    REQUIRE(1.0 == test(0U,0U));
+    REQUIRE(3.0 == test(0U,2U));
+    REQUIRE(4.0 == test(1U,0U));
+    REQUIRE(6.0 == test(1U,2U));
+
+    // linear access
+    REQUIRE(4.0 == test[3]);
+
+    // assignment
+    test[{1,2}] = 42.23;
+    REQUIRE(42.23 == (test[{1,2}])); // why are the parentheses necessary? bracket should have precedence over equality
+
+    test[3] = 13.37;
+    REQUIRE(13.37 == test[3]);
+
+    // get row
+    // auto second_row = test.slice(0, 1);
+    // REQUIRE(13.37 == second_row[0])
+
+    // boolean
+    MultiArray<bool, 3> test2({2,3,4});
+    std::cout << std::boolalpha << test2 << std::endl;
+
+    // open questions:
+    // - how to react to dimension mismatches?
+    // - allow runtime variable number of dimensions?
+    // - copy and mutability
+
+    // use with units
+    //using namespace units;
+    //using namespace units::physical;
+    //using namespace units::physical::si;
+    //using namespace units::physical::si;
+    //using namespace units::physical::si::literals;
+    //Length<kilometre> auto blub= 34.5_q_km;
+    //std::cout << quantity_cast<si::metre>(2.0_q_km) << ", " << 24.3 << std::endl;
+    //auto with_units = MultiArray(std::vector<si::length<si::kilometre>>(2.0_q_km, 2.5_q_km, 3.3_q_km), std::array<size_t, 2>{1,3});
+    //std::cout << with_units << std::endl;
+}
+
+TEST_CASE("MultiArray.floatOrder3Offsets", "[MultiArray]") {
+    std::vector<float> elements(100);
+    std::iota(elements.begin(), elements.end(), 0);
+    MultiArray<float, 3> test(elements, {2, 3, 4}, {14,4,1}, {0,0,0});
+    std::cout << test << std::endl;
+    // test that internal fields where set correctly
+    REQUIRE(2 == test.n(0));
+    REQUIRE(3 == test.n(1));
+    REQUIRE(4 == test.n(2));
+    REQUIRE(14 == test.stride(0));
+    REQUIRE(4 == test.stride(1));
+    REQUIRE(1 == test.stride(2));
+    REQUIRE(0 == test.offset(0));
+    REQUIRE(0 == test.offset(1));
+    REQUIRE(0 == test.offset(2));
+    // multi index access
+    REQUIRE(0.0f == test(0U,0U,0U));
+    REQUIRE(8.0f == test(0U,2U,0U));
+    REQUIRE(14.0f == test(1U,0U,0U));
+    REQUIRE(22.0f == test(1U,2U,0U));
+    REQUIRE(3.0f == test(0U,0U,3U));
+    REQUIRE(11.0f == test(0U,2U,3U));
+    REQUIRE(17.0f == test(1U,0U,3U));
+    REQUIRE(25.0f == test(1U,2U,3U));
+}
