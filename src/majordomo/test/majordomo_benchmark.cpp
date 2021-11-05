8600efe145c4827394714663532fa71df850b0b0
Frank Osterfeld <frank.osterfeld@kdab.com>
2021-11-05 09:27:51 +0100

    Benchmark fixes
    Use always inproc between broker and worker, use separate contexts
    for client and broker/worker when using TCP between client and broker.
    Print results in the end to not have them mixed with debug output.

--- src/majordomo/test/majordomo_benchmark.cpp
+++ src/majordomo/test/majordomo_benchmark.cpp
@@ -62,17 +62,29 @@ enum class Get {
     Async
 };
 
-void simple_one_worker_benchmark(std::string router_address, Get mode, int iterations, std::size_t payload_size) {
+struct Result {
+    std::string router_address;
+    int iterations;
+    Get mode;
+    std::size_t payload_size;
+    std::chrono::duration<double> duration;
+};
+
+Result simple_one_worker_benchmark(std::string router_address, Get mode, int iterations, std::size_t payload_size) {
+    const auto worker_router = std::string_view("inproc://for_worker");
+
     yaz::Context context;
     Broker broker("benchmarkbroker", "", context);
     REQUIRE(broker.bind(router_address, Broker::BindOption::Router));
+    REQUIRE(broker.bind(worker_router, Broker::BindOption::Router));
     RunInThread broker_run(broker);
 
     Worker worker(std::string(payload_size, '\xab'), context, "blob");
-    REQUIRE(worker.connect(router_address));
+    REQUIRE(worker.connect(worker_router));
     RunInThread worker_run(worker);
 
-    TestClient client(context);
+    yaz::Context client_context;
+    TestClient client(router_address.starts_with("inproc") ? context : client_context);
     REQUIRE(client.connect(router_address));
 
     const auto before = std::chrono::system_clock::now();
@@ -97,30 +109,35 @@ void simple_one_worker_benchmark(std::string router_address, Get mode, int itera
     const auto after = std::chrono::system_clock::now();
     const std::chrono::duration<double> diff = after - before;
 
-    std::cout << fmt::format("{}: {}. Payload {} bytes: {} iterations took {}s ({} messages/s)\n",
-                             router_address,
-                             mode == Get::Async ? "ASYNC" : "SYNC",
-                             payload_size,
-                             iterations,
-                             diff.count(),
-                             std::round(iterations / diff.count()));
+    Result r;
+    r.router_address = router_address;
+    r.mode = mode;
+    r.payload_size = payload_size;
+    r.iterations = iterations;
+    r.duration = diff;
+    return r;
 }
 
 void simple_two_worker_benchmark(std::string router_address, Get mode, int iterations, std::size_t payload1_size, std::size_t payload2_size) {
+    const auto worker_router = std::string_view("inproc://for_worker");
+
     yaz::Context context;
     Broker broker("benchmarkbroker", "", context);
     REQUIRE(broker.bind(router_address, Broker::BindOption::Router));
+    REQUIRE(broker.bind(worker_router, Broker::BindOption::Router));
     RunInThread broker_run(broker);
 
     Worker worker1(std::string(payload1_size, '\xab'), context, "blob1");
-    REQUIRE(worker1.connect(router_address));
+    REQUIRE(worker1.connect(worker_router));
+
     RunInThread worker1_run(worker1);
 
     Worker worker2(std::string(payload2_size, '\xab'), context, "blob2");
     REQUIRE(worker2.connect(router_address));
     RunInThread worker2_run(worker2);
 
-    TestClient client(context);
+    yaz::Context client_context;
+    TestClient client(router_address.starts_with("inproc") ? context : client_context);
     REQUIRE(client.connect(router_address));
 
     const auto before = std::chrono::system_clock::now();
@@ -160,11 +177,23 @@ int main(int argc, char **argv) {
     const auto tcp = std::string("tcp://127.0.0.1:12346");
     const auto inproc = std::string("inproc://benchmark");
 
-    simple_one_worker_benchmark(tcp, Get::Async, N, 10);
-    simple_one_worker_benchmark(inproc, Get::Async, N, 10);
-    simple_one_worker_benchmark(tcp, Get::Sync, N, 10);
-    simple_one_worker_benchmark(inproc, Get::Sync, N, 10);
+    std::vector<Result> results;
+
+    results.push_back(simple_one_worker_benchmark(tcp, Get::Async, N, 10));
+    results.push_back(simple_one_worker_benchmark(inproc, Get::Async, N, 10));
+    results.push_back(simple_one_worker_benchmark(tcp, Get::Sync, N, 10));
+    results.push_back(simple_one_worker_benchmark(inproc, Get::Sync, N, 10));
     //simple_one_worker_benchmark(tcp, Get::Async, 3000, 1024);
     //simple_one_worker_benchmark(tcp, Get::Async, 3000, 1024 * 1024);
-    simple_two_worker_benchmark(inproc, Get::Async, 10000, 10, 10);
+    //simple_two_worker_benchmark(inproc, Get::Async, 10000, 10, 10);
+
+    for (const auto &result : results) {
+        std::cout << fmt::format("{}: {}. Payload {} bytes: {} iterations took {}s ({} messages/s)\n",
+                                 result.router_address,
+                                 result.mode == Get::Async ? "ASYNC" : "SYNC",
+                                 result.payload_size,
+                                 result.iterations,
+                                 result.duration.count(),
+                                 std::round(result.iterations / result.duration.count()));
+    }
 }
