0b024a9767f36284b561659f9a1cfc490c075f10
Alexander Krimm <A.Krimm@gsi.de>
2021-11-26 16:46:40 +0100

    Finnish Json Serialiser

--- src/serialiser/test/JsonSerialiser_tests.cpp
+++ src/serialiser/test/JsonSerialiser_tests.cpp
@@ -38,16 +38,26 @@ struct DataX {
 // following is the visitor-pattern-macro that allows the compile-time reflections via refl-cpp
 ENABLE_REFLECTION_FOR(DataX, byteValue, shortValue, intValue, longValue, floatValue, doubleValue, stringValue, constStringValue, doubleArray, floatVector, doubleMatrix, nested)
 
-TEST_CASE("JsonDeserialisation", "[JsonClassSerialiser]") {
+struct SimpleInner {
+    std::string val1;
+    std::string val2;
+};
+ENABLE_REFLECTION_FOR(SimpleInner, val1, val2)
+struct Simple {
+    std::shared_ptr<SimpleInner> test;
+};
+ENABLE_REFLECTION_FOR(Simple, test)
+
+TEST_CASE("JsonDeserialisation", "[JsonSerialiser]") {
     opencmw::debug::resetStats();
     {
         opencmw::IoBuffer buffer;
-        auto              cars_json = R"({ "test":[ { "val1":1, "val2":2 }, { "val1":1, "val2":2 } ] })";
+        auto              cars_json = R"({ "test": { "val1":"foo", "val2":"bar"}})";
         buffer.reserve_spare(strlen(cars_json));
         buffer.putRaw(cars_json);
         std::cout << "Prepared json data: " << buffer.asString() << std::endl;
-        DataX foo;
-        auto  result = opencmw::deserialise<opencmw::Json, opencmw::ProtocolCheck::LENIENT>(buffer, foo);
+        Simple foo;
+        auto   result = opencmw::deserialise<opencmw::Json, opencmw::ProtocolCheck::LENIENT>(buffer, foo);
         std::cout << "deserialised: \n";
         for (auto e : result.exceptions) {
             std::cout << " ! " << e.what() << std::endl;
@@ -55,12 +65,14 @@ TEST_CASE("JsonDeserialisation", "[JsonClassSerialiser]") {
         for (auto f : result.additionalFields) {
             std::cout << " + " << std::get<0>(f) << std::endl;
         }
+        REQUIRE(foo.test.get()->val1 == "foo");
+        REQUIRE(foo.test.get()->val2 == "bar");
     }
     REQUIRE(opencmw::debug::dealloc == opencmw::debug::alloc); // a memory leak occurred
     opencmw::debug::resetStats();
 }
 
-TEST_CASE("JsonSerialisation", "[JsonClassSerialiser]") {
+TEST_CASE("JsonSerialisation", "[JsonSerialiser]") {
     opencmw::debug::resetStats();
     {
         opencmw::IoBuffer buffer;
@@ -72,6 +84,68 @@ TEST_CASE("JsonSerialisation", "[JsonClassSerialiser]") {
     }
     REQUIRE(opencmw::debug::dealloc == opencmw::debug::alloc); // a memory leak occurred
     opencmw::debug::resetStats();
+}
+
+TEST_CASE("isNumber", "[JsonSerialiser]") {
+    using namespace opencmw::json;
+    REQUIRE(isJsonNumberChar('0'));
+    REQUIRE(isJsonNumberChar('1'));
+    REQUIRE(isJsonNumberChar('2'));
+    REQUIRE(isJsonNumberChar('3'));
+    REQUIRE(isJsonNumberChar('4'));
+    REQUIRE(isJsonNumberChar('5'));
+    REQUIRE(isJsonNumberChar('6'));
+    REQUIRE(isJsonNumberChar('7'));
+    REQUIRE(isJsonNumberChar('8'));
+    REQUIRE(isJsonNumberChar('9'));
+    REQUIRE(isJsonNumberChar('+'));
+    REQUIRE(isJsonNumberChar('-'));
+    REQUIRE(isJsonNumberChar('e'));
+    REQUIRE(isJsonNumberChar('E'));
+    REQUIRE(isJsonNumberChar('.'));
+    REQUIRE_FALSE(isJsonNumberChar('a'));
+}
+TEST_CASE("readJsonString", "[JsonSerialiser]") {
+    using namespace opencmw;
+    using namespace opencmw::json;
+    {
+        IoBuffer buffer;
+        buffer.putRaw(R"""("test String 123 " sfef)""");
+        REQUIRE(readJsonString(buffer) == R"""(test String 123 )""");
+    }
+    // todo: make escape sequences work properly, we cannot return string views but have to copy into our own buffer
+    // for now we focus on non escaped strings first
+    // {
+    //     IoBuffer buffer;
+    //     buffer.putRaw("\"Hello\t\\\"special\\\"\nWorld!\"");
+    //     REQUIRE(readJsonString(buffer) == "Hello\t\"special\"\nWorld!");
+    // }
+}
+
+TEST_CASE("isWhitespace", "[JsonSerialiser]") {
+    using namespace opencmw;
+    using namespace opencmw::json;
+    REQUIRE(isJsonWhitespace(' '));
+    REQUIRE(isJsonWhitespace('\t'));
+    REQUIRE(isJsonWhitespace('\n'));
+    REQUIRE(isJsonWhitespace('\r'));
+    REQUIRE_FALSE(isJsonWhitespace('a'));
+}
+
+TEST_CASE("consumeWhitespace", "[JsonSerialiser]") {
+    using namespace opencmw;
+    using namespace opencmw::json;
+    IoBuffer buffer;
+    buffer.putRaw("\n \t345 \r \t\tbcdef");
+    REQUIRE(buffer.position() == 0);
+    consumeJsonWhitespace(buffer);
+    REQUIRE(buffer.position() == 3);
+    buffer.set_position(5);
+    consumeJsonWhitespace(buffer);
+    REQUIRE(buffer.position() == 5);
+    buffer.set_position(6);
+    consumeJsonWhitespace(buffer);
+    REQUIRE(buffer.position() == 11);
 }
 
 #pragma clang diagnostic pop
