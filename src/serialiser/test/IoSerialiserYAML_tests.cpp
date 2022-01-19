aeedaf74fd3df50dc68f15b295c1b6198a746546
Alexander Krimm <A.Krimm@gsi.de>
2022-01-19 18:53:18 +0100

    Add test for bool type and make class names unique
    Class names where not unique to the test confusing refl-cpp when making
    changes to the file. Also bool fields where added to the domain object
    for test coverage.

--- src/serialiser/test/IoSerialiserYAML_tests.cpp
+++ src/serialiser/test/IoSerialiserYAML_tests.cpp
@@ -6,12 +6,10 @@
 #include <string_view>
 #include <Utils.hpp>
 
-#include <units/isq/si/electric_current.h>
 #include <units/isq/si/energy.h>
 #include <units/isq/si/length.h>
 #include <units/isq/si/mass.h>
 #include <units/isq/si/resistance.h>
-#include <units/isq/si/speed.h>
 #include <units/isq/si/time.h>
 
 using namespace opencmw;
@@ -27,7 +25,7 @@ using opencmw::ExternalModifier::RW;
 using opencmw::ExternalModifier::RW_DEPRECATED;
 using opencmw::ExternalModifier::RW_PRIVATE;
 
-struct NestedData {
+struct NestedDataY {
     Annotated<int8_t, length<metre>, "nested int8_t">                          annByteValue   = 11;
     Annotated<int16_t, si::time<second>, "custom description for int16_t">     annShortValue  = 12;
     Annotated<int32_t, NoUnit, "custom description for int32_t">               annIntValue    = 13;
@@ -37,14 +35,16 @@ struct NestedData {
     Annotated<std::string, NoUnit, "custom description for string">            annStringValue = std::string("nested string");
     Annotated<std::array<double, 10>, NoUnit>                                  annDoubleArray = std::array<double, 10>{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     Annotated<std::vector<float>, NoUnit>                                      annFloatVector = std::vector{ 0.1f, 1.1f, 2.1f, 3.1f, 4.1f, 5.1f, 6.1f, 8.1f, 9.1f, 9.1f };
+    Annotated<std::array<bool, 4>, NoUnit, "description for bool array!">      annBoolArray   = std::array<bool, 4>{ true, false, true, true };
 
     // some default operator
-    auto operator<=>(const NestedData &) const = default;
+    auto operator<=>(const NestedDataY &) const = default;
 };
 // following is the visitor-pattern-macro that allows the compile-time reflections via refl-cpp
-ENABLE_REFLECTION_FOR(NestedData, annByteValue, annShortValue, annIntValue, annLongValue, annFloatValue, annDoubleValue, annStringValue, annDoubleArray, annFloatVector)
+ENABLE_REFLECTION_FOR(NestedDataY, annByteValue, annShortValue, annIntValue, annLongValue, annFloatValue, annDoubleValue, annStringValue, annDoubleArray, annFloatVector, annBoolArray)
 
 struct DataY {
+    bool                               boolValue        = false;
     int8_t                             byteValue        = 1;
     int16_t                            shortValue       = 2;
     int32_t                            intValue         = 3;
@@ -57,7 +57,7 @@ struct DataY {
     std::array<double, 10> const       constDoubleArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std::vector<float>                 floatVector      = { 0.1F, 1.1F, 2.1F, 3.1F, 4.1F, 5.1F, 6.1F, 8.1F, 9.1F, 9.1F };
     opencmw::MultiArray<double, 2>     doubleMatrix{ { 1, 3, 7, 4, 2, 3 }, { 2, 3 } };
-    NestedData                         nestedData;
+    NestedDataY                        nestedData;
     Annotated<double, resistance<ohm>> annotatedValue = 0.1;
     //
     // Annotated<std::map<std::string, std::string>, NoUnit, "a simple std::map"> map = {{"key1","value1"},{"key2","value2"},{"key3","value3"},{"key4","value4"}};
@@ -67,7 +67,7 @@ struct DataY {
     bool                               operator==(const DataY &) const = default;
 };
 // following is the visitor-pattern-macro that allows the compile-time reflections via refl-cpp
-ENABLE_REFLECTION_FOR(DataY, byteValue, shortValue, intValue, longValue, floatValue, doubleValue, stringValue, constStringValue, doubleArray, floatVector, /*doubleMatrix,*/ nestedData, annotatedValue, map, smallMap)
+ENABLE_REFLECTION_FOR(DataY, boolValue, byteValue, shortValue, intValue, longValue, floatValue, doubleValue, stringValue, constStringValue, doubleArray, floatVector, /*doubleMatrix,*/ nestedData, annotatedValue, map, smallMap)
 
 template<opencmw::SerialiserProtocol protocol, opencmw::ReflectableClass T>
 void checkSerialiserIdentity(opencmw::IoBuffer &buffer, const T &a, T &b) {
@@ -105,6 +105,7 @@ TEST_CASE("basic YAML serialisation", "[IoClassSerialiserYAML]") {
 
         DataY data2;
         REQUIRE(data == data2);
+        data2.boolValue      = true;
         data2.byteValue      = 30;
         data2.shortValue     = 3;
         data2.stringValue    = "change me";
@@ -118,7 +119,8 @@ TEST_CASE("basic YAML serialisation", "[IoClassSerialiserYAML]") {
         data2.nestedData.annDoubleArray[3] = 99;
         //        data2.doubleMatrix(0U, 0U)         = 42;
         data2.nestedData.annFloatVector.clear();
-        data2.map["key7"] = "value7";
+        data2.nestedData.annBoolArray[2] = false;
+        data2.map["key7"]                = "value7";
         data2.smallMap.clear();
         REQUIRE(data != data2);
 
@@ -138,9 +140,12 @@ TEST_CASE("basic YAML serialisation", "[IoClassSerialiserYAML]") {
         std::cout << "after: " << std::flush;
         diffView(std::cout, data, data2);
         REQUIRE(data == data2);
-        //
-        //        REQUIRE(data.doubleMatrix(0U, 0U) == data2.doubleMatrix(0U, 0U));
-        //        REQUIRE(data.doubleMatrix(1U, 2U) == data2.doubleMatrix(1U, 2U));
+
+        // REQUIRE(data.doubleMatrix(0U, 0U) == data2.doubleMatrix(0U, 0U));
+        // REQUIRE(data.doubleMatrix(1U, 2U) == data2.doubleMatrix(1U, 2U));
+
+        // std::cout << "yaml:\n" << std::flush;
+        // std::cout << buffer.asString() << std::endl;
     }
     REQUIRE(opencmw::debug::dealloc == opencmw::debug::alloc); // a memory leak occurred
     debug::resetStats();
