744f1d16cd685ec0af8ea1a7c9618f7e861448ec
Alexander Krimm <A.Krimm@gsi.de>
2022-01-25 09:58:13 +0100

    IoSerialiserCmwLight implementation
    Implement a CmwLight serialiser which supports the original CMW
    serialisation format. The only difference is, that it adds a char type
    which is not supported in cmw and some types might not be supported.

--- src/serialiser/include/IoSerialiserCmwLight.hpp
+++ src/serialiser/include/IoSerialiserCmwLight.hpp
@@ -2,37 +2,319 @@
 #define OPENCMW_CMWLIGHTSERIALISER_H
 
 #include "IoSerialiser.hpp"
-#include <list>
-#include <map>
-#include <queue>
+#include <string_view>
 
 #pragma clang diagnostic push
 #pragma ide diagnostic   ignored "cppcoreguidelines-avoid-magic-numbers"
 #pragma ide diagnostic   ignored "cppcoreguidelines-avoid-c-arrays"
 
 namespace opencmw {
+
 struct CmwLight : Protocol<"CmwLight"> {
 };
 
 namespace cmwlight {
 
+void skipString(IoBuffer &buffer);
+
+template<typename T>
+inline void skipArray(IoBuffer &buffer) {
+    if constexpr (is_stringlike<T>) {
+        for (auto i = buffer.get<int32_t>(); i > 0; i--) {
+            skipString(buffer);
+        }
+    } else {
+        buffer.skip(buffer.get<int32_t>() * static_cast<int32_t>(sizeof(T)));
+    }
 }
 
+inline void skipString(IoBuffer &buffer) { skipArray<char>(buffer); }
+
+template<typename T>
+inline void skipMultiArray(IoBuffer &buffer) {
+    buffer.skip(buffer.get<int32_t>() * static_cast<int32_t>(sizeof(int32_t))); // skip size header
+    skipArray<T>(buffer);                                                       // skip elements
+}
+
+template<typename T>
+inline int getTypeId() {
+    return IoSerialiser<CmwLight, T>::getDataTypeId();
+}
+template<typename T>
+inline int getTypeIdVector() {
+    return IoSerialiser<CmwLight, std::vector<T>>::getDataTypeId();
+}
+template<typename T, size_t N>
+inline int getTypeIdMultiArray() {
+    return IoSerialiser<CmwLight, MultiArray<T, N>>::getDataTypeId();
+}
+
+} // namespace cmwlight
+
+template<typename T>
+struct IoSerialiser<CmwLight, T> {
+    inline static constexpr uint8_t getDataTypeId() { return 0xFF; } // default value
+};
+
 template<Number T>
-struct IoSerialiser<T, CmwLight> { // catch all template
-    constexpr static bool serialise(IoBuffer &buffer, FieldDescription auto &field, const T &value) noexcept {
-        if (std::is_constant_evaluated()) {
-            using namespace std::literals;
-            buffer.put(field);
-            buffer.template put(value);
-            return true;
-        }
-        std::cout << fmt::format("{} - serialise-catch-all: {} {} value: {} - constexpr?: {}\n",
-                CmwLight::protocolName(), typeName<T>, field.fieldName, value,
-                std::is_constant_evaluated());
-        return false;
+struct IoSerialiser<CmwLight, T> {
+    inline static constexpr uint8_t getDataTypeId() {
+        // clang-format off
+        if      constexpr (std::is_same_v<bool   , T>) { return 0; }
+        else if constexpr (std::is_same_v<int8_t , T>) { return 1; }
+        else if constexpr (std::is_same_v<int16_t, T>) { return 2; }
+        else if constexpr (std::is_same_v<int32_t, T>) { return 3; }
+        else if constexpr (std::is_same_v<int64_t, T>) { return 4; }
+        else if constexpr (std::is_same_v<float  , T>) { return 5; }
+        else if constexpr (std::is_same_v<double , T>) { return 6; }
+        else if constexpr (std::is_same_v<char   , T>) { return 201; }
+        else { static_assert(opencmw::always_false<T>); }
+        // clang-format on
+    }
+
+    constexpr static void serialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, const T &value) noexcept {
+        buffer.put(value);
+    }
+
+    constexpr static void deserialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, T &value) noexcept {
+        value = buffer.get<T>();
     }
 };
+
+template<StringLike T>
+struct IoSerialiser<CmwLight, T> {
+    inline static constexpr uint8_t getDataTypeId() { return 7; }
+
+    constexpr static void           serialise(IoBuffer &buffer, FieldDescription auto const           &/*field*/, const T &value) noexcept {
+        buffer.put(value);
+    }
+
+    constexpr static void deserialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, T &value) noexcept {
+        value = buffer.get<T>();
+    }
+};
+
+template<ArrayOrVector T>
+struct IoSerialiser<CmwLight, T> {
+    using MemberType = typename T::value_type;
+
+    inline static constexpr uint8_t getDataTypeId() {
+        // clang-format off
+        if      constexpr (std::is_same_v<bool   , MemberType>) { return  9; }
+        else if constexpr (std::is_same_v<int8_t , MemberType>) { return 10; }
+        else if constexpr (std::is_same_v<int16_t, MemberType>) { return 11; }
+        else if constexpr (std::is_same_v<int32_t, MemberType>) { return 12; }
+        else if constexpr (std::is_same_v<int64_t, MemberType>) { return 13; }
+        else if constexpr (std::is_same_v<float  , MemberType>) { return 14; }
+        else if constexpr (std::is_same_v<double , MemberType>) { return 15; }
+        else if constexpr (std::is_same_v<char   , MemberType>) { return 202; }
+        else if constexpr (opencmw::is_stringlike<MemberType> ) { return 16; }
+        else { static_assert(opencmw::always_false<T>); }
+        // clang-format on
+    }
+
+    constexpr static void serialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, const T &value) noexcept {
+        buffer.put(value);
+    }
+
+    constexpr static void deserialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, T &value) noexcept {
+        buffer.getArray(value);
+    }
+};
+
+template<MultiArrayType T>
+struct IoSerialiser<CmwLight, T> {
+    inline static constexpr uint8_t getDataTypeId() {
+        // clang-format off
+        if      constexpr (std::is_same_v<bool   , typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<int8_t , typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<int16_t, typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<int32_t, typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<int64_t, typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<float  , typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<double , typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<char   , typename T::value_type> && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (opencmw::is_stringlike<typename T::value_type>  && T::n_dims_ == 1) { return cmwlight::getTypeIdVector<typename T::value_type>(); }
+        else if constexpr (std::is_same_v<bool   , typename T::value_type> && T::n_dims_ == 2) { return 17; }
+        else if constexpr (std::is_same_v<int8_t , typename T::value_type> && T::n_dims_ == 2) { return 18; }
+        else if constexpr (std::is_same_v<int16_t, typename T::value_type> && T::n_dims_ == 2) { return 19; }
+        else if constexpr (std::is_same_v<int32_t, typename T::value_type> && T::n_dims_ == 2) { return 20; }
+        else if constexpr (std::is_same_v<int64_t, typename T::value_type> && T::n_dims_ == 2) { return 21; }
+        else if constexpr (std::is_same_v<float  , typename T::value_type> && T::n_dims_ == 2) { return 22; }
+        else if constexpr (std::is_same_v<double , typename T::value_type> && T::n_dims_ == 2) { return 23; }
+        else if constexpr (std::is_same_v<char   , typename T::value_type> && T::n_dims_ == 2) { return 203; }
+        else if constexpr (opencmw::is_stringlike<typename T::value_type>  && T::n_dims_ == 2) { return 24; }
+        else if constexpr (std::is_same_v<bool   , typename T::value_type>) { return 25; }
+        else if constexpr (std::is_same_v<int8_t , typename T::value_type>) { return 26; }
+        else if constexpr (std::is_same_v<int16_t, typename T::value_type>) { return 27; }
+        else if constexpr (std::is_same_v<int32_t, typename T::value_type>) { return 28; }
+        else if constexpr (std::is_same_v<int64_t, typename T::value_type>) { return 29; }
+        else if constexpr (std::is_same_v<float  , typename T::value_type>) { return 30; }
+        else if constexpr (std::is_same_v<double , typename T::value_type>) { return 31; }
+        else if constexpr (std::is_same_v<char   , typename T::value_type>) { return 204; }
+        else if constexpr (opencmw::is_stringlike<typename T::value_type> ) { return 32; }
+        else { static_assert(opencmw::always_false<T>); }
+        // clang-format on
+    }
+
+    constexpr static void serialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, const T &value) noexcept {
+        std::array<int32_t, T::n_dims_> dims;
+        for (uint32_t i = 0U; i < T::n_dims_; i++) {
+            dims[i] = static_cast<int32_t>(value.dimensions()[i]);
+        }
+        buffer.put(dims);
+        buffer.put(value.elements()); // todo: account for strides and offsets (possibly use iterators?)
+    }
+
+    constexpr static void deserialise(IoBuffer &buffer, FieldDescription auto const & /*field*/, T &value) noexcept {
+        const std::array<int32_t, T::n_dims_> dimWire = buffer.getArray<int32_t, T::n_dims_>();
+        for (auto i = 0U; i < T::n_dims_; i++) {
+            value.dimensions()[i] = static_cast<typename T::size_t_>(dimWire[i]);
+        }
+        value.element_count()        = value.dimensions()[T::n_dims_ - 1];
+        value.stride(T::n_dims_ - 1) = 1;
+        value.offset(T::n_dims_ - 1) = 0;
+        for (auto i = T::n_dims_ - 1; i > 0; i--) {
+            value.element_count() *= value.dimensions()[i - 1];
+            value.stride(i - 1) = value.stride(i) * value.dimensions()[i];
+            value.offset(i - 1) = 0;
+        }
+        buffer.getArray(value.elements());
+    }
+};
+
+template<>
+struct IoSerialiser<CmwLight, START_MARKER> {
+    inline static constexpr uint8_t getDataTypeId() { return 0xFC; }
+
+    constexpr static void           serialise(IoBuffer &buffer, FieldDescription auto const &field, const START_MARKER           &/*value*/) noexcept {
+        buffer.put(static_cast<int32_t>(field.subfields));
+    }
+
+    constexpr static void deserialise(IoBuffer & /*buffer*/, FieldDescription auto const & /*field*/, const START_MARKER &) {
+        // do not do anything, as the start marker is of size zero and only the type byte is important
+    }
+};
+
+template<>
+struct IoSerialiser<CmwLight, END_MARKER> {
+    inline static constexpr uint8_t getDataTypeId() { return 0xFE; }
+
+    static void                     serialise(IoBuffer                     &/*buffer*/, FieldDescription auto const                     &/*field*/, const END_MARKER                     &/*value*/) noexcept {
+        // do not do anything, as the end marker is of size zero and only the type byte is important
+    }
+
+    constexpr static void deserialise(IoBuffer & /*buffer*/, FieldDescription auto const & /*field*/, const END_MARKER &) {
+        // do not do anything, as the end marker is of size zero and only the type byte is important
+    }
+};
+
+template<>
+struct IoSerialiser<CmwLight, OTHER> {
+    inline static constexpr uint8_t getDataTypeId() { return 0xFD; }
+
+    static void                     serialise(IoBuffer                     &/*buffer*/, FieldDescription auto const                     &/*field*/, const END_MARKER                     &/*value*/) noexcept {
+        // do not do anything, as the end marker is of size zero and only the type byte is important
+    }
+
+    static void deserialise(IoBuffer &buffer, FieldDescription auto const &field, const OTHER &) {
+        using namespace cmwlight;
+        auto typeId = field.intDataType;
+        // clang-format off
+        // primitives
+             if (typeId == getTypeId<bool       >()) { buffer.skip(sizeof(bool   )); }
+        else if (typeId == getTypeId<int8_t     >()) { buffer.skip(sizeof(int8_t )); }
+        else if (typeId == getTypeId<int16_t    >()) { buffer.skip(sizeof(int16_t)); }
+        else if (typeId == getTypeId<int32_t    >()) { buffer.skip(sizeof(int32_t)); }
+        else if (typeId == getTypeId<int64_t    >()) { buffer.skip(sizeof(int64_t)); }
+        else if (typeId == getTypeId<float      >()) { buffer.skip(sizeof(float  )); }
+        else if (typeId == getTypeId<double     >()) { buffer.skip(sizeof(double )); }
+        else if (typeId == getTypeId<char       >()) { buffer.skip(sizeof(char   )); }
+        else if (typeId == getTypeId<std::string>()) { skipString(buffer); }
+        // arrays
+        else if (typeId == getTypeIdVector<int8_t     >()) { skipArray<int8_t     >(buffer); }
+        else if (typeId == getTypeIdVector<int16_t    >()) { skipArray<int16_t    >(buffer); }
+        else if (typeId == getTypeIdVector<int32_t    >()) { skipArray<int32_t    >(buffer); }
+        else if (typeId == getTypeIdVector<int64_t    >()) { skipArray<int64_t    >(buffer); }
+        else if (typeId == getTypeIdVector<float      >()) { skipArray<float      >(buffer); }
+        else if (typeId == getTypeIdVector<double     >()) { skipArray<double     >(buffer); }
+        else if (typeId == getTypeIdVector<char       >()) { skipArray<char       >(buffer); }
+        else if (typeId == getTypeIdVector<std::string>()) { skipArray<std::string>(buffer); }
+        // 2D and Multi array
+        else if (typeId == getTypeIdMultiArray<int8_t     , 2>() || typeId == getTypeIdMultiArray<int8_t     , 3>()) { skipMultiArray<int8_t     >(buffer); }
+        else if (typeId == getTypeIdMultiArray<int16_t    , 2>() || typeId == getTypeIdMultiArray<int16_t    , 3>()) { skipMultiArray<int16_t    >(buffer); }
+        else if (typeId == getTypeIdMultiArray<int32_t    , 2>() || typeId == getTypeIdMultiArray<int32_t    , 3>()) { skipMultiArray<int32_t    >(buffer); }
+        else if (typeId == getTypeIdMultiArray<int64_t    , 2>() || typeId == getTypeIdMultiArray<int64_t    , 3>()) { skipMultiArray<int64_t    >(buffer); }
+        else if (typeId == getTypeIdMultiArray<float      , 2>() || typeId == getTypeIdMultiArray<float      , 3>()) { skipMultiArray<float      >(buffer); }
+        else if (typeId == getTypeIdMultiArray<double     , 2>() || typeId == getTypeIdMultiArray<double     , 3>()) { skipMultiArray<double     >(buffer); }
+        else if (typeId == getTypeIdMultiArray<char       , 2>() || typeId == getTypeIdMultiArray<char       , 3>()) { skipMultiArray<char       >(buffer); }
+        else if (typeId == getTypeIdMultiArray<std::string, 2>() || typeId == getTypeIdMultiArray<std::string, 3>()) { skipMultiArray<std::string>(buffer); }
+        // nested
+        // unsupported
+        else { throw ProtocolException(fmt::format("Skipping data type {} is not supported", typeId)); }
+        // clang-format on
+    }
+};
+
+template<>
+struct FieldHeaderWriter<CmwLight> {
+    template<const bool /*writeMetaInfo*/, typename DataType>
+    constexpr std::size_t static put(IoBuffer &buffer, FieldDescription auto const &field, const DataType &data) {
+        using StrippedDataType = std::remove_reference_t<decltype(getAnnotatedMember(unwrapPointer(data)))>;
+        if constexpr (is_same_v<DataType, END_MARKER>) { // do not put endMarker type id into buffer
+            return std::numeric_limits<size_t>::max();
+        }
+        constexpr auto dataTypeSize = static_cast<int32_t>(sizeof(StrippedDataType));
+        buffer.reserve_spare(((field.fieldName.size() + 2UL) * sizeof(uint8_t)) + dataTypeSize);
+        if (field.hierarchyDepth != 0) {
+            buffer.put<IoBuffer::MetaInfo::WITH>(field.fieldName); // full field name with zero termination
+        }
+        if constexpr (!is_same_v<DataType, START_MARKER>) {                        // do not put startMarker type id into buffer
+            buffer.put(IoSerialiser<CmwLight, StrippedDataType>::getDataTypeId()); // data type ID
+        }
+        IoSerialiser<CmwLight, StrippedDataType>::serialise(buffer, field, getAnnotatedMember(unwrapPointer(data)));
+        return std::numeric_limits<size_t>::max();
+    }
+};
+
+template<>
+struct FieldHeaderReader<CmwLight> {
+    template<ProtocolCheck protocolCheckVariant>
+    inline static void get(IoBuffer &buffer, DeserialiserInfo & /*info*/, FieldDescription auto &field) {
+        field.headerStart     = buffer.position();
+        field.dataEndPosition = std::numeric_limits<size_t>::max();
+        field.modifier        = ExternalModifier::UNKNOWN;
+        if (field.subfields == 0) {
+            field.intDataType = IoSerialiser<CmwLight, END_MARKER>::getDataTypeId();
+            field.hierarchyDepth--;
+            field.dataStartPosition = buffer.position();
+            return;
+        }
+        if (field.subfields == -1) {
+            if (field.hierarchyDepth != 0) {                      // do not read field description for root element
+                field.fieldName = buffer.get<std::string_view>(); // full field name
+            }
+            field.intDataType = IoSerialiser<CmwLight, START_MARKER>::getDataTypeId();
+            field.subfields   = static_cast<int16_t>(buffer.get<int32_t>());
+        } else {
+            field.fieldName   = buffer.get<std::string_view>(); // full field name
+            field.intDataType = buffer.get<uint8_t>();          // data type ID
+            field.subfields--;                                  // decrease the number of remaining fields in the structure... todo: adapt strategy for nested fields (has to somewhere store subfields)
+        }
+        field.dataStartPosition = buffer.position();
+    }
+};
+
+template<>
+inline DeserialiserInfo checkHeaderInfo<CmwLight>(IoBuffer &buffer, DeserialiserInfo info, const ProtocolCheck /*protocolCheckVariant*/) {
+    // cmw has no specific header to check, we could try to find a common pattern in all cmw output to match to that
+    // minimal thing to do is to check the number of fields to be non-zero. N.B. this fails for YaS data
+    if (buffer.at<int32_t>(buffer.position()) == 0) {
+        throw ProtocolException("Illegal number of members"); // todo: respect protocol check type
+    }
+    return info;
+}
+
 } // namespace opencmw
 
 #pragma clang diagnostic pop
---
+++ src/serialiser/test/IoSerialiserCmwLight_tests.cpp
@@ -0,0 +1,169 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic   ignored "LoopDoesntUseConditionVariableInspection"
+#pragma ide diagnostic   ignored "cppcoreguidelines-avoid-magic-numbers"
+
+#include <catch2/catch.hpp>
+#include <Debug.hpp>
+#include <IoSerialiserCmwLight.hpp>
+#include <iostream>
+#include <string_view>
+#include <Utils.hpp>
+
+using namespace std::literals;
+
+using opencmw::Annotated;
+using opencmw::NoUnit;
+using opencmw::ProtocolCheck;
+using opencmw::ExternalModifier::RO;
+using opencmw::ExternalModifier::RW;
+using opencmw::ExternalModifier::RW_DEPRECATED;
+using opencmw::ExternalModifier::RW_PRIVATE;
+
+namespace ioserialiser_cmwlight_test {
+struct SimpleTestData {
+    int                             a   = 1337;
+    float                           ab  = 13.37f;
+    double                          abc = 42.23;
+    std::string                     b   = "hello";
+    std::array<int, 3>              c{ 3, 2, 1 };
+    std::vector<double>             cd{ 2.3, 3.4, 4.5, 5.6 };
+    std::vector<std::string>        ce{ "hello", "world" };
+    opencmw::MultiArray<double, 2>  d{ { 1, 2, 3, 4, 5, 6 }, { 2, 3 } };
+    std::unique_ptr<SimpleTestData> e = nullptr;
+    bool                            operator==(const ioserialiser_cmwlight_test::SimpleTestData &other) const { // deep comparison function
+        return a == other.a && ab == other.ab && abc == other.abc && b == other.b && c == other.c && cd == other.cd && d == other.d && ((!e && !other.e) || *e == *(other.e));
+    }
+};
+} // namespace ioserialiser_cmwlight_test
+ENABLE_REFLECTION_FOR(ioserialiser_cmwlight_test::SimpleTestData, a, ab, abc, b, c, cd, ce, d, e)
+
+TEST_CASE("IoClassSerialiserCmwLight simple test", "[IoClassSerialiser]") {
+    using namespace opencmw;
+    using namespace opencmw::utils; // for operator<< and fmt::format overloading
+    using namespace ioserialiser_cmwlight_test;
+    debug::resetStats();
+    {
+        debug::Timer timer("IoClassSerialiser basic syntax", 30);
+
+        IoBuffer     buffer;
+        std::cout << fmt::format("buffer size (before): {} bytes\n", buffer.size());
+
+        SimpleTestData data{
+            .a   = 30,
+            .ab  = 1.2f,
+            .abc = 1.23,
+            .b   = "abc",
+            .c   = { 5, 4, 3 },
+            .cd  = { 2.1, 4.2 },
+            .ce  = { "hallo", "welt" },
+            .d   = { { 6, 5, 4, 3, 2, 1 }, { 3, 2 } },
+            .e   = std::make_unique<SimpleTestData>(SimpleTestData{
+                      .a   = 40,
+                      .ab  = 2.2f,
+                      .abc = 2.23,
+                      .b   = "abcdef",
+                      .c   = { 9, 8, 7 },
+                      .cd  = { 3.1, 1.2 },
+                      .ce  = { "ei", "gude" },
+                      .d   = { { 6, 5, 4, 3, 2, 1 }, { 3, 2 } },
+                      .e   = nullptr })
+        };
+
+        // check that empty buffer cannot be deserialised
+        buffer.put(0L);
+        CHECK_THROWS_AS((opencmw::deserialise<opencmw::CmwLight, ProtocolCheck::LENIENT>(buffer, data)), ProtocolException);
+        buffer.clear();
+
+        SimpleTestData data2;
+        REQUIRE(data != data2);
+        std::cout << "object (short): " << ClassInfoShort << data << '\n';
+        std::cout << fmt::format("object (fmt): {}\n", data);
+        std::cout << "object (long):  " << ClassInfoVerbose << data << '\n';
+
+        opencmw::serialise<opencmw::CmwLight>(buffer, data);
+        std::cout << fmt::format("buffer size (after): {} bytes\n", buffer.size());
+
+        buffer.put("a\0df"sv); // add some garbage after the serialised object to check if it is handled correctly
+
+        buffer.reset();
+        auto result = opencmw::deserialise<opencmw::CmwLight, ProtocolCheck::LENIENT>(buffer, data2);
+        std::cout << "deserialised object (long):  " << ClassInfoVerbose << data2 << '\n';
+        std::cout << "deserialisation messages: " << result << std::endl;
+        REQUIRE(data == data2);
+    }
+    REQUIRE(opencmw::debug::dealloc == opencmw::debug::alloc); // a memory leak occurred
+    debug::resetStats();
+}
+
+namespace ioserialiser_cmwlight_test {
+struct SimpleTestDataMoreFields {
+    int                             a2   = 1336;
+    float                           ab2  = 13.36f;
+    double                          abc2 = 42.22;
+    std::string                     b2   = "bonjour";
+    std::array<int, 3>              c2{ 7, 8, 9 };
+    std::vector<double>             cd2{ 2.4, 3.6, 4.8, 5.0 };
+    std::vector<std::string>        ce2{ "hello", "world" };
+    opencmw::MultiArray<double, 2>  d2{ { 4, 5, 6, 7, 8, 9 }, { 2, 3 } };
+    std::unique_ptr<SimpleTestData> e2  = nullptr;
+    int                             a   = 1337;
+    float                           ab  = 13.37f;
+    double                          abc = 42.23;
+    std::string                     b   = "hello";
+    std::array<int, 3>              c{ 3, 2, 1 };
+    std::vector<double>             cd{ 2.3, 3.4, 4.5, 5.6 };
+    std::vector<std::string>        ce{ "hello", "world" };
+    opencmw::MultiArray<double, 2>  d{ { 1, 2, 3, 4, 5, 6 }, { 2, 3 } };
+    bool                            operator==(const SimpleTestDataMoreFields &) const = default;
+    std::unique_ptr<SimpleTestData> e                                                  = nullptr;
+};
+} // namespace ioserialiser_cmwlight_test
+ENABLE_REFLECTION_FOR(ioserialiser_cmwlight_test::SimpleTestDataMoreFields, a2, ab2, abc2, b2, c2, cd2, ce2, d2, e2, a, ab, abc, b, c, cd, ce, d, e)
+
+#pragma clang diagnostic pop
+TEST_CASE("IoClassSerialiserCmwLight missing field", "[IoClassSerialiser]") {
+    using namespace opencmw;
+    using namespace opencmw::utils; // for operator<< and fmt::format overloading
+    using namespace ioserialiser_cmwlight_test;
+    debug::resetStats();
+    {
+        debug::Timer timer("IoClassSerialiser basic syntax", 30);
+
+        IoBuffer     buffer;
+        std::cout << fmt::format("buffer size (before): {} bytes\n", buffer.size());
+
+        SimpleTestData data{
+            .a   = 30,
+            .ab  = 1.2f,
+            .abc = 1.23,
+            .b   = "abc",
+            .c   = { 5, 4, 3 },
+            .cd  = { 2.1, 4.2 },
+            .ce  = { "hallo", "welt" },
+            .d   = { { 6, 5, 4, 3, 2, 1 }, { 3, 2 } }
+        };
+        SimpleTestDataMoreFields data2;
+        std::cout << fmt::format("object (fmt): {}\n", data);
+        opencmw::serialise<opencmw::CmwLight>(buffer, data);
+        buffer.reset();
+        auto result = opencmw::deserialise<opencmw::CmwLight, ProtocolCheck::LENIENT>(buffer, data2);
+        std::cout << fmt::format("deserialised object (fmt): {}\n", data2);
+        std::cout << "deserialisation messages: " << result << std::endl;
+        REQUIRE(result.setFields["root"] == std::vector<bool>{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 });
+        REQUIRE(result.additionalFields.empty());
+        REQUIRE(result.exceptions.empty());
+
+        std::cout << "\nand now the other way round!\n\n";
+        buffer.clear();
+        opencmw::serialise<opencmw::CmwLight>(buffer, data2);
+        buffer.reset();
+        auto result_back = opencmw::deserialise<opencmw::CmwLight, ProtocolCheck::LENIENT>(buffer, data);
+        std::cout << fmt::format("deserialised object (fmt): {}\n", data);
+        std::cout << "deserialisation messages: " << result_back << std::endl;
+        REQUIRE(result_back.setFields["root"] == std::vector<bool>{ 1, 1, 1, 1, 1, 1, 1, 1, 0 });
+        REQUIRE(result_back.additionalFields.size() == 8);
+        REQUIRE(result_back.exceptions.size() == 8);
+    }
+    REQUIRE(opencmw::debug::dealloc == opencmw::debug::alloc); // a memory leak occurred
+    debug::resetStats();
+}
