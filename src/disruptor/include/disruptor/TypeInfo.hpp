12f858680b16008bd992097132a5a42dbb91af28
Restyled.io <commits@restyled.io>
2022-03-10 18:57:39 +0000

    Restyled by clang-format

--- src/disruptor/include/disruptor/TypeInfo.hpp
+++ src/disruptor/include/disruptor/TypeInfo.hpp
@@ -31,8 +31,8 @@ public:
     bool                  operator==(const TypeInfo &rhs) const { return intrinsicTypeInfo() == rhs.intrinsicTypeInfo(); }
 
     static std::string    dotNetify(const std::string &typeName) {
-           std::regex pattern("::");
-           return std::regex_replace(typeName, pattern, ".");
+        std::regex pattern("::");
+        return std::regex_replace(typeName, pattern, ".");
     }
     static std::string unqualifyName(const std::string &fullyQualifiedName) {
         auto position = fullyQualifiedName.rfind('.');
@@ -86,4 +86,3 @@ public:
         return hash<type_index>()(type_index(value.intrinsicTypeInfo()));
     }
 };
-
