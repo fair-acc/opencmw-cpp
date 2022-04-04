cf984f8bbd2b0213b3ecdaf1aa45f327620fb44f
Ralph J. Steinhagen <r.steinhagen@gsi.de>
2022-04-04 07:15:55 +0200

    added non-multiplexed BasicSetting implementation
    N.B. idea inspired by David Rowland's () and Fabian Renn-Giles's (@hogliux) "Real-Time 101" talk at Meeting C++ 2019, https://youtu.be/ndeN983j_GQ

---
+++ src/disruptor/test/Setting_tests.cpp
@@ -0,0 +1,51 @@
+#include <catch2/catch.hpp>
+
+#include <BasicSetting.hpp>
+
+TEST_CASE("BasicSetting Real-Time tests", "[BasicSetting]") {
+    using namespace opencmw;
+    using opencmw::MutableOption::RealTimeMutable;
+    BasicSetting<int, RealTimeMutable> setting;
+
+    {
+        auto guardedValue = setting.accessGuard<AccessType::RealTime>();
+        REQUIRE(guardedValue == 0);
+        REQUIRE(!std::is_const_v<std::remove_reference_t<decltype(guardedValue.get())>>);
+        guardedValue = 40;
+        guardedValue += 2;
+        REQUIRE(guardedValue == 42);
+    }
+    setting.replace<AccessType::RealTime>(43);
+
+    {
+        auto guardedValue = setting.accessGuard<AccessType::NonRealTime>();
+        REQUIRE(guardedValue == 43);
+        REQUIRE(std::is_const_v<std::remove_reference_t<decltype(guardedValue.get())>>);
+        // guardedValue = 43; // should not compile
+        REQUIRE(guardedValue == 43);
+    }
+}
+
+TEST_CASE("BasicSetting Non-Real-Time tests", "[BasicSetting]") {
+    using namespace opencmw;
+    using opencmw::MutableOption::NonRealTimeMutable;
+    BasicSetting<int, NonRealTimeMutable> setting;
+
+    {
+        auto guardedValue = setting.accessGuard<AccessType::NonRealTime>();
+        REQUIRE(guardedValue == 0);
+        REQUIRE(!std::is_const_v<std::remove_reference_t<decltype(guardedValue.get())>>);
+        guardedValue = 42;
+        REQUIRE(guardedValue == 42);
+    }
+
+    setting.replace<AccessType::NonRealTime>(43);
+
+    {
+        auto guardedValue = setting.accessGuard<AccessType::RealTime>();
+        REQUIRE(guardedValue == 43);
+        REQUIRE(std::is_const_v<std::remove_reference_t<decltype(guardedValue.get())>>);
+        // guardedValue = 43; // should not compile
+        REQUIRE(guardedValue == 43);
+    }
+}
