51fd6ea335bb7e2f5d52b51049c476d41ea27114
Alexander Busse <alex@busse.earth>
2023-06-25 23:16:07 +0200

    Non-distributed DSNS with persistence #37

---
+++ src/services/services.hpp
@@ -0,0 +1,96 @@
+#ifndef OPENCMW_CPP_SERVICES_HPP
+#define OPENCMW_CPP_SERVICES_HPP
+
+#include <majordomo/Broker.hpp>
+#include <majordomo/RestBackend.hpp>
+
+#include <thread>
+
+namespace opencmw {
+namespace services {
+
+class RunDefaultBroker {
+protected:
+    majordomo::Broker<>                _broker{ "Broker", {} };
+    std::vector<std::jthread>          _threads;
+    std::vector<std::function<void()>> _shutdowns;
+    std::mutex                         _mutex;
+    std::mutex                         _setup;
+
+public:
+    template<typename WorkerT, typename HandlerT>
+    void runWorker() {
+        std::lock_guard         m{ _setup };
+        std::mutex              foo;
+        std::condition_variable cv;
+        bool                    ready{ false };
+
+        _threads.push_back(std::move(std::jthread{
+                [&]() {
+                    WorkerT w{ this->_broker, HandlerT{} };
+                    {
+                        std::lock_guard guard{ _mutex };
+                        this->_shutdowns.push_back([&w]() { w.shutdown(); });
+                    }
+                    ready = true;
+                    cv.notify_all();
+                    w.run();
+                } }));
+        std::unique_lock lock{ foo };
+        cv.wait(lock, [&ready] { return ready; });
+    }
+
+    ~RunDefaultBroker() {
+        std::lock_guard m{ _setup };
+        std::lock_guard guard{ _mutex };
+        _broker.shutdown();
+        std::for_each(_shutdowns.rbegin(), _shutdowns.rend(), [](auto &t) { t(); });
+
+        std::for_each(_threads.rbegin(), _threads.rend(), [](auto &t) { t.join(); });
+    }
+
+    RunDefaultBroker() {
+        std::lock_guard         m{ _setup };
+        std::condition_variable cv;
+        std::mutex              _m;
+        std::unique_lock        lock{ _m };
+        bool                    ready{ false };
+
+        _threads.push_back(std::move(std::jthread{ [this, &cv, &ready] {
+            std::string                                                 rootPath{ "./" };
+            auto                                                        fs = cmrc::assets::get_filesystem();
+            majordomo::RestBackend<majordomo::PLAIN_HTTP, decltype(fs)> backend{ _broker, fs };
+            {
+                std::lock_guard guard{ _mutex };
+                this->_shutdowns.push_back([&backend]() { backend.shutdown(); });
+            }
+            ready = true;
+            cv.notify_all();
+            backend.run();
+        } }));
+
+        cv.wait(lock, [&ready] { return ready; });
+    }
+
+    void startBroker() {
+        std::lock_guard         l{ _setup };
+        std::lock_guard         ltt{ _mutex };
+        std::condition_variable cv;
+        std::mutex              _m;
+        std::unique_lock        lock{ _m };
+        bool                    ready{ false };
+
+        _threads.push_back(std::jthread{
+                [this, &cv, &ready] {
+                    ready = true;
+                    cv.notify_all();
+                    _broker.run();
+                } });
+        cv.wait(lock, [&ready] { return ready; });
+    }
+};
+
+}
+} // namespace opencmw::services
+
+#endif // OPENCMW_CPP_SERVICES_HPP
