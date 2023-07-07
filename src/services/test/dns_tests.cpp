3f99becc84d31da3030dba1ff4a354664f37e371
Restyled.io <commits@restyled.io>
2023-07-07 07:32:10 +0000

    Restyled by clang-format

--- src/services/test/dns_tests.cpp
+++ src/services/test/dns_tests.cpp
@@ -1,7 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells the catch header to generate a main
 #define CATCH_CONFIG_ENABLE_BENCHMARKING 1
 
-
 #include <catch2/catch.hpp>
 #include <filesystem>
 #include <services/dns.hpp>
