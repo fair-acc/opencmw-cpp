952cba0ac05fdb79c9c0ca20cbef4dae9a9616f3
Ralph J. Steinhagen <r.steinhagen@gsi.de>
2023-10-08 08:28:14 +0200

    bumped fmt version and fixed formatter

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -18,7 +18,7 @@ FetchContent_Declare(
 FetchContent_Declare(
         fmt
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-        GIT_TAG 10.0.0
+        GIT_TAG 10.1.1
 )
 
 # dependency of mp-units, building examples, tests, etc is off by default
