8b472fd43051badf49ddb2693bee633527abdaba
Magnus Groß <21310755+vimpostor@users.noreply.github.com>
2024-11-12 11:38:14 +0100

    Implement OAuth Client (#357)
     for details see: https://github.com/fair-acc/opencmw-cpp/issues/350

---
+++ src/services/include/services/OAuthClient.hpp
@@ -0,0 +1,276 @@
+#ifndef OPENCMW_CPP_OAUTHCLIENT_HPP
+#define OPENCMW_CPP_OAUTHCLIENT_HPP
+
+#include "IoBuffer.hpp"
+#include "IoSerialiserJson.hpp"
+#include "URI.hpp"
+#include <httplib.h>
+#include <majordomo/Worker.hpp>
+
+namespace opencmw {
+
+using StrictUri = opencmw::URI<opencmw::uri_check::STRICT>;
+
+struct OAuthContext {
+    std::string secretToken;
+};
+
+struct OAuthInput {
+    std::string scope;
+    std::string clientId;
+    std::string clientSecret;
+    std::string secret;
+};
+
+struct OAuthOutput {
+    std::string authorizationUri;
+    std::string secret;
+    std::string accessToken;
+    std::string refreshToken;
+};
+} // namespace opencmw
+
+ENABLE_REFLECTION_FOR(opencmw::OAuthContext, secretToken)
+ENABLE_REFLECTION_FOR(opencmw::OAuthInput, scope, clientId, clientSecret, secret)
+ENABLE_REFLECTION_FOR(opencmw::OAuthOutput, authorizationUri, secret, accessToken, refreshToken)
+
+namespace opencmw {
+
+class Token {
+public:
+    std::string                                                       _token;
+    std::optional<std::chrono::time_point<std::chrono::steady_clock>> _expiry; // time when this token will expire if any
+
+    Token(const std::string &token = "")
+        : _token(token) {}
+    Token(const std::string &token, std::chrono::seconds expiresIn)
+        : Token(token) {
+        // convenience constructor for OAuth access token responses (expires_in is always in seconds)
+        _expiry = std::chrono::steady_clock::now() + expiresIn;
+    }
+
+    bool expired() const {
+        return _expiry && *_expiry < std::chrono::steady_clock::now();
+    }
+
+    bool valid() const {
+        return !_token.empty() && !expired();
+    }
+};
+
+struct OAuthAccess {
+    Token accessToken;
+    Token refreshToken;
+};
+
+struct OAuthResponse {
+    int         status = -1;
+    std::string body;
+};
+
+// response as per https://datatracker.ietf.org/doc/html/rfc6749#section-5.1
+struct AccessTokenResponse {
+    std::string access_token;
+    std::string token_type;
+    int         expires_in = 3600;
+    std::string refresh_token;
+    std::string bullshit;
+};
+} // namespace opencmw
+
+ENABLE_REFLECTION_FOR(opencmw::AccessTokenResponse, access_token, token_type, expires_in, refresh_token, bullshit)
+
+namespace opencmw {
+
+// This class implements an RFC 6749 compliant OAuth client
+// https://datatracker.ietf.org/doc/html/rfc6749
+class OAuthClient {
+private:
+    StrictUri                              _redirectUri;
+    StrictUri                              _endpoint;
+    StrictUri                                                              _tokenEndpoint;
+    httplib::Server                        _srv;
+    std::unique_ptr<std::thread>                                           _thread;
+    std::function<void(const std::string &code, const std::string &state)> _endpointCallback;
+
+public:
+    explicit OAuthClient(StrictUri redirectUri, StrictUri endpoint, StrictUri tokenEndpoint)
+        : _redirectUri(redirectUri), _endpoint(endpoint), _tokenEndpoint(tokenEndpoint) {
+        _srv.Get("/", [&](const httplib::Request req, httplib::Response &res) {
+            std::string code, state;
+            // response as per https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.2
+            for (const auto &[k, v] : req.params) {
+                if (k == "code") {
+                    code = v;
+                } else if (k == "state") {
+                    state = v;
+                }
+            }
+            if (code.empty()) {
+                res.set_content("Did not receive an RFC 6749-compliant response.", "text/plain");
+                res.status = 401; // Unauthorized
+            } else {
+                res.set_content("Authorization complete. You can close this browser window now.\n", "text/plain");
+                if (_endpointCallback) {
+                    _endpointCallback(code, state);
+                }
+            }
+        });
+        _thread = std::make_unique<std::thread>([this]() {
+            _srv.listen(*_redirectUri.hostName(), *_redirectUri.port());
+        });
+    }
+
+    // convenience API function for clients who just want to open the URI in a web browser
+    static bool openWebBrowser(const StrictUri &uri) {
+        fmt::println("Opening {} in a web browser...", uri.str());
+#ifdef __unix__
+        // An alternative would be to use the org.freedesktop.portal.OpenURI method, but that would require dbus
+        // and it is more likely that xdg-open is present, than the portal running
+        // flawfinder: ignore
+        return !std::system(fmt::format("xdg-open '{}'", uri.str()).c_str());
+#else
+        std::cout << "Your platform is unsupported, please open the link manually." << std::endl;
+        return false;
+#endif
+    }
+
+    void setEndpointCallback(decltype(_endpointCallback) callback) {
+        _endpointCallback = callback;
+    }
+
+    StrictUri authorizationUri(const std::string &scope, const std::string &clientId, const std::string &state = "", const std::string &clientSecret = "") const {
+        // parameters as per https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.1
+        std::unordered_map<std::string, std::optional<std::string>> params{ { "scope", scope }, { "response_type", "code" }, { "client_id", clientId }, { "redirect_uri", _redirectUri.str() } };
+
+        if (!state.empty()) {
+            params.emplace("state", state);
+        }
+
+        if (!clientSecret.empty()) {
+            params.emplace("client_secret", clientSecret);
+        }
+
+        return StrictUri::UriFactory(_endpoint).setQuery(params).build();
+    }
+
+    static std::optional<httplib::Client> getClient(const StrictUri &endpoint) {
+        const auto scheme   = endpoint.scheme();
+        const auto hostname = endpoint.hostName();
+        const auto port     = endpoint.port();
+        const auto path     = endpoint.path();
+        if (!scheme || !hostname || !port || !path || path->empty()) {
+            return std::nullopt;
+        }
+
+        return httplib::Client(*scheme + "://" + *hostname + ":" + std::to_string(*port));
+    }
+
+    OAuthAccess getAccessToken(const httplib::Params &params) {
+        auto client = getClient(_tokenEndpoint);
+        if (!client) {
+            return {};
+        }
+
+        auto res = client->Post(*_tokenEndpoint.path(), params);
+        if (!res || res->status != 200) {
+            return {};
+        }
+        opencmw::IoBuffer   buf{ res->body.c_str() };
+        AccessTokenResponse resp;
+        opencmw::deserialise<opencmw::Json, opencmw::ProtocolCheck::LENIENT>(buf, resp);
+        if (resp.access_token.empty()) {
+            return {};
+        }
+
+        return { Token(resp.access_token, std::chrono::seconds(resp.expires_in)), Token(resp.refresh_token) };
+    }
+
+    OAuthAccess requestToken(const std::string &authCode, const std::string &clientId) {
+        if (authCode.empty() || clientId.empty()) {
+            return {};
+        }
+
+        // parameters as per https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
+        return getAccessToken({ { "grant_type", "authorization_code" }, { "code", authCode }, { "redirect_uri", _redirectUri.str() }, { "client_id", clientId } });
+    }
+
+    OAuthAccess refreshAccessToken(const std::string &refreshToken, const std::string &clientId) {
+        if (refreshToken.empty()) {
+            return {};
+        }
+        // parameters as per https://datatracker.ietf.org/doc/html/rfc6749#section-6
+        return getAccessToken({ { "grant_type", "refresh_token" }, { "refresh_token", refreshToken }, { "client_id", clientId } });
+    }
+
+    void stop() {
+        _srv.stop();
+        _thread->join();
+    }
+};
+
+namespace {
+using OAuthWorkerType = majordomo::Worker<"/oauth", OAuthContext, OAuthInput, OAuthOutput, majordomo::description<"Authorization with OAuth2">>;
+};
+
+template<typename T>
+T makeRandom(uint32_t size) {
+    std::random_device               rd;
+    std::mt19937                     generator(rd());
+    std::uniform_int_distribution<T> distribution(0, static_cast<T>(size));
+
+    return distribution(generator);
+}
+
+template<>
+inline std::string makeRandom(uint32_t size) {
+    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    std::string randomString(size, ' ');
+    std::generate(randomString.begin(), randomString.end(), [&]() {
+        return characters[makeRandom<uint32_t>(static_cast<uint32_t>(characters.length() - 1))];
+    });
+
+    return randomString;
+}
+
+class OAuthWorker : public OAuthWorkerType {
+public:
+    explicit OAuthWorker(StrictUri redirectUri, StrictUri endpoint, StrictUri tokenEndpoint, StrictUri brokerAddress, const zmq::Context &context, majordomo::Settings settings = {})
+        : OAuthWorkerType(brokerAddress, {}, context, settings), _client(redirectUri, endpoint, tokenEndpoint) { init(); };
+    template<typename BrokerType>
+    explicit OAuthWorker(StrictUri redirecturi, StrictUri endpoint, StrictUri tokenEndpoint, const BrokerType &broker)
+        : OAuthWorkerType(broker, {}), _client(redirecturi, endpoint, tokenEndpoint) { init(); };
+
+    void stop() {
+        _client.stop();
+    }
+
+private:
+    std::map<std::string, std::string> _secrets;
+    OAuthClient                        _client;
+    void                               init() {
+        _client.setEndpointCallback([this](const std::string &code, const std::string &state) {
+            _secrets[state] = code;
+        });
+        OAuthWorkerType::setCallback([this](const majordomo::RequestContext &rawCtx, const OAuthContext &context, const OAuthInput &in, OAuthContext &replyContext, OAuthOutput &out) {
+            if (in.secret.empty()) {
+                // first stage, client gets a secret in the response back plus an URI to authorize at
+                constexpr std::size_t secretLength = 64;
+                const auto            secret       = makeRandom<std::string>(secretLength);
+                out.authorizationUri               = _client.authorizationUri(in.scope, in.clientId, secret).str();
+                out.secret                         = secret;
+            } else if (_secrets.contains(in.secret)) {
+                // second stage, client has authorized and can get an access token
+                const auto code   = _secrets.at(in.secret);
+                const auto access = _client.requestToken(code, in.clientId);
+                out.accessToken   = access.accessToken._token;
+                out.refreshToken  = access.refreshToken._token;
+            }
+        });
+    }
+};
+
+} // namespace opencmw
+
+#endif // OPENCMW_CPP_OAUTHCLIENT_HPP
---
+++ src/services/test/OAuthClient_tests.cpp
@@ -0,0 +1,145 @@
+#define CATCH_CONFIG_MAIN // This tells the catch header to generate a main
+
+#include <catch2/catch.hpp>
+
+#ifndef __EMSCRIPTEN__
+
+#include <Client.hpp>
+#include <concepts/majordomo/helpers.hpp>
+#include <services/OAuthClient.hpp>
+
+namespace opencmw_oauth_client_test {
+
+// Usually you want to open an external browser for the authorization flow.
+// But this instead simulates a browser with a httplib client,
+// so that the test can be run headless in CI
+bool loginAtUri(const opencmw::URI<opencmw::STRICT> &uri) {
+    std::cout << "Simulating to login with a browser at " << uri.str() << std::endl;
+
+    auto browser     = opencmw::OAuthClient::getClient(uri);
+    auto relativeRef = uri.relativeRef();
+    REQUIRE(browser);
+    REQUIRE(relativeRef);
+
+    // We have to include the params in the request path
+    httplib::Result res = browser->Get(*relativeRef);
+    REQUIRE(res);
+    // Now the cursed part begins, first find the HTML submit URL of the credentials form
+    const auto begin = res->body.find("action=\"");
+    REQUIRE(begin != std::string::npos);
+    // search for the enclosing ", luckily every " inside the URI is URI-encoded
+    const auto end = res->body.find("\"", begin + 8);
+    REQUIRE(end != std::string::npos);
+    auto action = res->body.substr(begin + 8, end - begin - 8);
+
+    // unescape every HTML escaped &
+    std::size_t n = 0;
+    while ((n = action.find("&amp;", n)) != std::string::npos) {
+        action.replace(n, 5, "&");
+    }
+
+    const opencmw::URI actionUri{ action };
+
+    // Of course we also have to simulate a cookie store, because otherwise Keycloak denies access
+    std::string cookies;
+    for (const auto &[k, v] : res->headers) {
+        if (k == "Set-Cookie") {
+            // to make parsing easier, remove all cookie metadata and just always send all cookies
+            const auto semicolon = v.find(';');
+            cookies              = cookies + (semicolon == std::string::npos ? v : v.substr(0, semicolon)) + "; ";
+        }
+    }
+    REQUIRE(cookies.size() > 2);
+    // According to https://datatracker.ietf.org/doc/html/rfc6265#section-4.1 cookies must not have a trailing semicolon
+    cookies.resize(cookies.size() - 2);
+
+    // Cookies ready, now hit the endpoint
+    httplib::Headers headers{ { { "Cookie", cookies } } };
+    relativeRef = actionUri.relativeRef();
+    REQUIRE(relativeRef);
+    httplib::Params params{ { { "username", "testuser" }, { "password", "testuser" }, { "credentialId", "" } } };
+
+    res = browser->Post(*relativeRef, headers, params);
+    REQUIRE(res);
+    REQUIRE(res->status == 302);
+    headers = res->headers;
+
+    // follow the redirect
+    const auto it = headers.find("Location");
+    REQUIRE(it != headers.cend());
+    const opencmw::URI redirecUri{ it->second };
+    auto               redirectClient = opencmw::OAuthClient::getClient(redirecUri);
+    relativeRef                       = redirecUri.relativeRef();
+    REQUIRE(relativeRef);
+#if 0
+    // TODO: Why does this crash?
+    // In theory we should be able to use httplib to call the redirect URI
+    res = redirectClient->Get(*relativeRef);
+    REQUIRE(res);
+#else
+    // call the final redirect URI with curl as a workaround
+    const std::string curl = "curl '" + it->second + "'";
+    // flawfinder: ignore
+    res->status            = std::system(curl.c_str()) + 200;
+#endif
+    REQUIRE(res->status == 200);
+
+    return true;
+}
+
+TEST_CASE("Worker test", "[OAuth]") {
+    opencmw::majordomo::Broker<> broker{ "/Broker", {} };
+    opencmw::OAuthWorker         oauthWorker{ opencmw::URI("http://localhost:8091"), opencmw::URI("http://localhost:8090/realms/testrealm/protocol/openid-connect/auth"), opencmw::URI("http://localhost:8090/realms/testrealm/protocol/openid-connect/token"), broker };
+
+    REQUIRE(broker.bind(opencmw::URI<>("mds://127.0.0.1:12345")));
+    REQUIRE(broker.bind(opencmw::URI<>("mdp://127.0.0.1:12346")));
+
+    RunInThread brokerThread(broker);
+    RunInThread oauthThread(oauthWorker);
+
+    REQUIRE(waitUntilWorkerServiceAvailable(broker.context, oauthWorker));
+
+    std::vector<std::unique_ptr<opencmw::client::ClientBase>> clients;
+    clients.emplace_back(std::make_unique<opencmw::client::MDClientCtx>(broker.context, 20ms, ""));
+    opencmw::client::ClientContext client{ std::move(clients) };
+
+    opencmw::IoBuffer              inBuf;
+    opencmw::OAuthInput            in{ "openid", "testclientid" };
+    opencmw::serialise<opencmw::YaS>(inBuf, in);
+
+    std::condition_variable cv;
+    std::mutex              m;
+    bool                    done = false;
+
+    client.set(opencmw::URI("mdp://127.0.0.1:12346/oauth"), [&](const mdp::Message &reply) {
+        opencmw::OAuthOutput out;
+        auto outBuf = reply.data;
+        opencmw::deserialise<opencmw::YaS, opencmw::ProtocolCheck::IGNORE>(outBuf, out);
+
+        REQUIRE(out.authorizationUri.size());
+        REQUIRE(out.secret.size());
+        REQUIRE(loginAtUri(opencmw::URI(out.authorizationUri)));
+
+        opencmw::IoBuffer iBuf;
+        opencmw::OAuthInput codeIn {"openid", "testclientid", "", out.secret};
+        opencmw::serialise<opencmw::YaS>(iBuf, codeIn);
+        client.set(opencmw::URI("mdp://127.0.0.1:12346/oauth"), [&](const mdp::Message& rep) {
+                opencmw::OAuthOutput tokenOut;
+                auto tokenOutBuf = rep.data;
+                opencmw::deserialise<opencmw::YaS, opencmw::ProtocolCheck::IGNORE>(tokenOutBuf, tokenOut);
+
+                // we must have got an access token now
+                const auto accessToken = tokenOut.accessToken;
+                REQUIRE(accessToken.size());
+                std::lock_guard lk(m);
+                done = true;
+                cv.notify_one();
+        }, std::move(iBuf)); }, std::move(inBuf));
+
+    std::unique_lock lk(m);
+    cv.wait(lk, [&] { return done; });
+    oauthWorker.stop();
+}
+
+} // namespace opencmw_oauth_client_test
+#endif
