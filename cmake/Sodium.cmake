c19bfc239431f56c2f4f6e0c0e4869a6b23ca926
Alexander Krimm <alex@wirew0rm.de>
2025-02-24 12:18:39 +0100

    CMake: fix CMakeWarnings and link statically against zeromq
    Fixes some easy to fix CMake Warnings to make the cmake generation less
    spammy.
    
    Also I noticed that up to now we were linking against a custom build of
    zeromq dynamically which would either require to also copy the
    libzeromq.so dynamic library or rely on the library on the target system
    to be binary identical to our internal build.
    This changes the linkage to static so we consistently link dynamically
    to system libraries and statically to the libraries we build as part of
    our build (openssl, sodium and zeromq at the moment).
    
    Also we directly use and build libsodium, but zeromq was replacing it
    with libtweetnacl, which conflicted at link time to this commit makes
    our build of libsodium usable by zeromq by exporting the appropriate
    variables.
    Since libsodium is not compiled as fPIC, the shared library build of
    zeromq had to be disabled as trying to link libzmq.so against the non
    fPIC libsodium caused linker errors.
    
    Signed-off-by: Alexander Krimm <alex@wirew0rm.de>

--- cmake/Sodium.cmake
+++ cmake/Sodium.cmake
@@ -1,6 +1,4 @@
 # libsodium does not have cmake support, so we need to add it ourself
-project("sodium" LANGUAGES C)
-
 FetchContent_Declare(
         libsodium
         GIT_REPOSITORY https://github.com/jedisct1/libsodium.git
@@ -12,14 +10,25 @@ FetchContent_MakeAvailable(libsodium)
 file(GLOB_RECURSE SODIUM_C "${libsodium_SOURCE_DIR}/src/*.c")
 file(GLOB_RECURSE SODIUM_H "${libsodium_SOURCE_DIR}/src/*.h")
 
-add_library(${PROJECT_NAME} ${SODIUM_C} ${SODIUM_H})
+add_library(sodium STATIC ${SODIUM_C} ${SODIUM_H})
 
-target_include_directories(${PROJECT_NAME} PUBLIC $<BUILD_INTERFACE:${libsodium_SOURCE_DIR}/src/libsodium/include> $<INSTALL_INTERFACE:include/opencmw> PRIVATE "${libsodium_SOURCE_DIR}/src/libsodium/include/sodium")
+target_include_directories(sodium PUBLIC $<BUILD_INTERFACE:${libsodium_SOURCE_DIR}/src/libsodium/include> $<INSTALL_INTERFACE:include/opencmw> PRIVATE "${libsodium_SOURCE_DIR}/src/libsodium/include/sodium")
 # silence warnings about being built by a different build system
-target_compile_definitions(${PROJECT_NAME} PRIVATE CONFIGURED)
+target_compile_definitions(sodium PRIVATE CONFIGURED)
+
+# allow zeromq to also use this version of sodium instead of using it's internal conflicting tweetnacl
+set(SODIUM_FOUND TRUE)
+set(SODIUM_INCLUDE_DIRS
+    "$<BUILD_INTERFACE:${libsodium_SOURCE_DIR}/src/libsodium/include>"
+    "$<INSTALL_INTERFACE:include/opencmw>"
+    "${libsodium_SOURCE_DIR}/src/libsodium/include/sodium"
+)
+set(SODIUM_LIBRARY "${CMAKE_CURRENT_BINARY_DIR}/libsodium.a")
+set(SODIUM_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/libsodium.a")
+set(SODIUM_VERSION "28.0")
 
 set(SODIUM_LIBRARY_VERSION_MAJOR 28)
 set(SODIUM_LIBRARY_VERSION_MINOR 0)
 configure_file("${libsodium_SOURCE_DIR}/src/libsodium/include/sodium/version.h.in" "${libsodium_SOURCE_DIR}/src/libsodium/include/sodium/version.h")
 
-install(TARGETS ${PROJECT_NAME} EXPORT opencmwTargets PUBLIC_HEADER DESTINATION include/opencmw)
+install(TARGETS sodium EXPORT opencmwTargets PUBLIC_HEADER DESTINATION include/opencmw)
