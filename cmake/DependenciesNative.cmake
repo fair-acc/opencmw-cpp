ec23b4f10a743ae7828ac1141551678fe6dfb51e
Alexander Krimm <A.Krimm@gsi.de>
2025-06-23 17:09:58 +0200

    CMake: use correct libdir for dependencies
    Replace hardcoded paths with cmake variables. Since these are also used
    in the upstream project, otherwise compilation will fail if they have a
    different value than what was hardcoded. (lib vs lib64)
    
    Signed-off-by: Alexander Krimm <A.Krimm@gsi.de>

--- cmake/DependenciesNative.cmake
+++ cmake/DependenciesNative.cmake
@@ -1,4 +1,5 @@
 include(ExternalProject)
+include(GNUInstallDirs)
 
 set(OPENSSL_C_FLAGS "-O3 -march=x86-64-v3" CACHE STRING "OpenSSL custom CFLAGS" FORCE)
 set(OPENSSL_CXX_FLAGS "-O3 -march=x86-64-v3" CACHE STRING "OpenSSL custom CXXFLAGS" FORCE)
@@ -37,7 +38,7 @@ ExternalProject_Add(Nghttp2Project
         GIT_REPOSITORY https://github.com/nghttp2/nghttp2
         GIT_TAG v1.65.0
         GIT_SHALLOW ON
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/nghttp2-install/lib/libnghttp2.a
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/nghttp2-install/${CMAKE_INSTALL_LIBDIR}/libnghttp2.a
         UPDATE_COMMAND ""
         CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/nghttp2-install
@@ -52,7 +53,7 @@ ExternalProject_Add(Nghttp2Project
 
 add_library(nghttp2-static STATIC IMPORTED GLOBAL)
 set_target_properties(nghttp2-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/nghttp2-install/lib/libnghttp2.a"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/nghttp2-install/${CMAKE_INSTALL_LIBDIR}/libnghttp2.a"
         INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/nghttp2-install/include"
 )
 add_dependencies(nghttp2-static Nghttp2Project)
@@ -61,7 +62,7 @@ ExternalProject_Add(Nghttp3Project
         GIT_REPOSITORY https://github.com/ngtcp2/nghttp3.git
         GIT_TAG v1.10.1
         GIT_SHALLOW ON
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/nghttp3-install/lib/libnghttp3.a
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/nghttp3-install/${CMAKE_INSTALL_LIBDIR}/libnghttp3.a
         UPDATE_COMMAND ""
         CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/nghttp3-install
@@ -78,7 +79,7 @@ ExternalProject_Add(NgTcp2Project
         GIT_TAG v1.13.0
         GIT_SHALLOW ON
         PREFIX ${CMAKE_BINARY_DIR}/ngtcp2-install
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/ngtcp2-install/lib/libngtcp2.a ${CMAKE_BINARY_DIR}/ngtcp2-install/lib/libngtcp2_crypto_ossl.a
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2.a ${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2_crypto_ossl.a
         UPDATE_COMMAND ""
         CMAKE_ARGS
         -DOPENSSL_ROOT_DIR:PATH=${OPENSSL_INSTALL_DIR}
@@ -94,21 +95,21 @@ ExternalProject_Add(NgTcp2Project
 
 add_library(ngtcp2-static STATIC IMPORTED GLOBAL)
 set_target_properties(ngtcp2-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/ngtcp2-install/lib/libngtcp2.a"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2.a"
         INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/ngtcp2-install/include"
 )
 add_dependencies(ngtcp2-static NgTcp2Project)
 
 add_library(ngtcp2-crypto-ossl-static STATIC IMPORTED GLOBAL)
 set_target_properties(ngtcp2-crypto-ossl-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/ngtcp2-install/lib/libngtcp2_crypto_ossl.a"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2_crypto_ossl.a"
         INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/ngtcp2-install/include"
 )
 add_dependencies(ngtcp2-crypto-ossl-static NgTcp2Project)
 
 add_library(nghttp3-static STATIC IMPORTED GLOBAL)
 set_target_properties(nghttp3-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/nghttp3-install/lib/libnghttp3.a"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/nghttp3-install/${CMAKE_INSTALL_LIBDIR}/libnghttp3.a"
         INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/nghttp3-install/include"
 )
 add_dependencies(nghttp3-static Nghttp3Project)
