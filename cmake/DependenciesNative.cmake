1600e15ecf3942781d07074dc8b1f28b1c968ca9
Alexander Krimm <A.Krimm@gsi.de>
2025-06-30 17:53:06 +0200

    CMake: put install prefix into CI cached directory
    Signed-off-by: Alexander Krimm <A.Krimm@gsi.de>

--- cmake/DependenciesNative.cmake
+++ cmake/DependenciesNative.cmake
@@ -3,7 +3,7 @@ include(GNUInstallDirs)
 
 set(OPENSSL_C_FLAGS "-O3 -march=x86-64-v3" CACHE STRING "OpenSSL custom CFLAGS" FORCE)
 set(OPENSSL_CXX_FLAGS "-O3 -march=x86-64-v3" CACHE STRING "OpenSSL custom CXXFLAGS" FORCE)
-set(OPENSSL_INSTALL_DIR "${CMAKE_BINARY_DIR}/openssl-install")
+set(OPENSSL_INSTALL_DIR "${CMAKE_BINARY_DIR}/_deps/openssl-install")
 
 # Build custom OpenSSL with QUIC support
 ExternalProject_Add(OpenSslProject
@@ -38,10 +38,10 @@ ExternalProject_Add(Nghttp2Project
         GIT_REPOSITORY https://github.com/nghttp2/nghttp2
         GIT_TAG v1.65.0
         GIT_SHALLOW ON
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/nghttp2-install/${CMAKE_INSTALL_LIBDIR}/libnghttp2.a
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/_deps/nghttp2-install/${CMAKE_INSTALL_LIBDIR}/libnghttp2.a
         UPDATE_COMMAND ""
         CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/nghttp2-install
+        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/_deps/nghttp2-install
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
         -DENABLE_LIB_ONLY:BOOL=ON
         -DENABLE_HTTP3:BOOL=OFF
@@ -53,8 +53,8 @@ ExternalProject_Add(Nghttp2Project
 
 add_library(nghttp2-static STATIC IMPORTED GLOBAL)
 set_target_properties(nghttp2-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/nghttp2-install/${CMAKE_INSTALL_LIBDIR}/libnghttp2.a"
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/nghttp2-install/include"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_deps/nghttp2-install/${CMAKE_INSTALL_LIBDIR}/libnghttp2.a"
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/_deps/nghttp2-install/include"
 )
 add_dependencies(nghttp2-static Nghttp2Project)
 
@@ -62,10 +62,10 @@ ExternalProject_Add(Nghttp3Project
         GIT_REPOSITORY https://github.com/ngtcp2/nghttp3.git
         GIT_TAG v1.10.1
         GIT_SHALLOW ON
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/nghttp3-install/${CMAKE_INSTALL_LIBDIR}/libnghttp3.a
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/_deps/nghttp3-install/${CMAKE_INSTALL_LIBDIR}/libnghttp3.a
         UPDATE_COMMAND ""
         CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/nghttp3-install
+        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/_deps/nghttp3-install
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
         -DENABLE_LIB_ONLY:BOOL=ON
         -DENABLE_DEBUG:BOOL=${ENABLE_NGHTTP_DEBUG}
@@ -78,13 +78,13 @@ ExternalProject_Add(NgTcp2Project
         GIT_REPOSITORY https://github.com/ngtcp2/ngtcp2.git
         GIT_TAG v1.13.0
         GIT_SHALLOW ON
-        PREFIX ${CMAKE_BINARY_DIR}/ngtcp2-install
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2.a ${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2_crypto_ossl.a
+        PREFIX ${CMAKE_BINARY_DIR}/_deps/ngtcp2-install
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/_deps/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2.a ${CMAKE_BINARY_DIR}/_deps/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2_crypto_ossl.a
         UPDATE_COMMAND ""
         CMAKE_ARGS
         -DOPENSSL_ROOT_DIR:PATH=${OPENSSL_INSTALL_DIR}
         -DENABLE_OPENSSL:BOOL=ON
-        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/ngtcp2-install
+        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/_deps/ngtcp2-install
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
         -DENABLE_LIB_ONLY:BOOL=ON
         -DENABLE_DEBUG:BOOL=${ENABLE_NGHTTP_DEBUG}
@@ -95,22 +95,22 @@ ExternalProject_Add(NgTcp2Project
 
 add_library(ngtcp2-static STATIC IMPORTED GLOBAL)
 set_target_properties(ngtcp2-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2.a"
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/ngtcp2-install/include"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_deps/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2.a"
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/_deps/ngtcp2-install/include"
 )
 add_dependencies(ngtcp2-static NgTcp2Project)
 
 add_library(ngtcp2-crypto-ossl-static STATIC IMPORTED GLOBAL)
 set_target_properties(ngtcp2-crypto-ossl-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2_crypto_ossl.a"
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/ngtcp2-install/include"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_deps/ngtcp2-install/${CMAKE_INSTALL_LIBDIR}/libngtcp2_crypto_ossl.a"
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/_deps/ngtcp2-install/include"
 )
 add_dependencies(ngtcp2-crypto-ossl-static NgTcp2Project)
 
 add_library(nghttp3-static STATIC IMPORTED GLOBAL)
 set_target_properties(nghttp3-static PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/nghttp3-install/${CMAKE_INSTALL_LIBDIR}/libnghttp3.a"
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/nghttp3-install/include"
+        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_deps/nghttp3-install/${CMAKE_INSTALL_LIBDIR}/libnghttp3.a"
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/_deps/nghttp3-install/include"
 )
 add_dependencies(nghttp3-static Nghttp3Project)
 
