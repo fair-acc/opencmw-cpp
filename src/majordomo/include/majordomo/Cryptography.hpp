4f2dc3585a053ea795bd16df47fd11ef03f025e9
Magnus Groß <magnus.gross+github@kdab.com>
2026-04-02 13:20:44 +0200

    Cryptography: Fix ODR violations
    Signed-off-by: Magnus Groß <magnus.gross+github@kdab.com>

--- src/majordomo/include/majordomo/Cryptography.hpp
+++ src/majordomo/include/majordomo/Cryptography.hpp
@@ -23,19 +23,19 @@ public:
     unsigned char hash[crypto_generichash_KEYBYTES];
 };
 
-std::pair<PublicKey, PrivateKey> generateKeyPair() {
+inline std::pair<PublicKey, PrivateKey> generateKeyPair() {
     std::pair<PublicKey, PrivateKey> result;
     crypto_sign_keypair(result.first.key, result.second.key);
     return result;
 }
 
-KeyHash publicKeyHash(const PublicKey &key) {
+inline KeyHash publicKeyHash(const PublicKey &key) {
     KeyHash result;
     crypto_generichash(result.hash, sizeof result.hash, key.key, crypto_sign_PUBLICKEYBYTES, nullptr, 0);
     return result;
 }
 
-std::string messageHash(const mdp::Message &msg) {
+inline std::string messageHash(const mdp::Message &msg) {
     unsigned char buf[crypto_generichash_KEYBYTES];
     for (size_t i = 0; i < crypto_generichash_KEYBYTES; ++i) {
         buf[i] = msg.rbac[i];
@@ -43,7 +43,7 @@ std::string messageHash(const mdp::Message &msg) {
     return std::string(buf, buf + crypto_generichash_KEYBYTES);
 }
 
-void sign(mdp::Message &msg, const PrivateKey &key, const KeyHash &hash) {
+inline void sign(mdp::Message &msg, const PrivateKey &key, const KeyHash &hash) {
     // write key hash followed by signature
     unsigned char buf[crypto_generichash_KEYBYTES + crypto_sign_BYTES];
     std::copy(hash.hash, hash.hash + crypto_generichash_KEYBYTES, buf);
@@ -53,7 +53,7 @@ void sign(mdp::Message &msg, const PrivateKey &key, const KeyHash &hash) {
     msg.rbac = rbac;
 }
 
-bool verify(mdp::Message &msg, const PublicKey &key) {
+inline bool verify(const mdp::Message &msg, const PublicKey &key) {
     auto          data = msg.data;
     unsigned char sig[crypto_sign_BYTES];
     for (size_t i = 0; i < crypto_sign_BYTES; ++i) {
@@ -62,7 +62,6 @@ bool verify(mdp::Message &msg, const PublicKey &key) {
     }
     return !crypto_sign_verify_detached(sig, data.data(), data.size(), key.key);
 }
-
 }
 
 } // namespace opencmw::majordomo::cryptography
